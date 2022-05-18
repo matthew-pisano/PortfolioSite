abe9fbafefc99604400c8acc4b4569c174daff35
Matthew Pisano <matthewpisano14@gmail.com>
2022-05-17 22:54:40 -0400

    reactReorg

--- client/src/App.js
+++ client/src/App.js
@@ -1,39 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {
+    BrowserRouter as Router,
+    Route,
+} from "react-router-dom";
+import Wrapper from './Wrapper';
+import { Routes } from 'react-router-dom';
 
 class App extends Component {
-  state = {
-    data: null
-  };
-
-  componentDidMount() {
-    this.callBackendAPI()
-      .then(res => this.setState({ data: res.express }))
-      .catch(err => console.log(err));
-  }
-    // fetching the GET route from the Express server which matches the GET route from server.js
-  callBackendAPI = async () => {
-    const response = await fetch('/express_backend');
-    const body = await response.json();
-
-    if (response.status !== 200) {
-      throw Error(body.message);
+    render() {
+        console.log("routing");
+        return (
+            <Router>
+                <Routes>
+                    <Route path={"/"} element={<Wrapper/>}/>
+                </Routes>
+            </Router>
+        );
     }
-    return body;
-  };
-
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">{this.state.data}</p>
-      </div>
-    );
-  }
 }
 
 export default App;
--- client/src/Wrapper.js
+++ client/src/Wrapper.js
@@ -1,12 +1,64 @@
 import React, { Component } from 'react';
-import Custom from './Custom';
 import Home from './Home';
 import Imperium from './Imperium';
 import Simplex from './Simplex';
+import * as common from './common';
 
 class Wrapper extends Component {
-
     render() {
+        let hierarchy = {
+            name: "public/",
+            subTree: [
+                {name: "home.html"},
+                {
+                    name: "personal/",
+                    subTree: [
+                        {name: "simplex.html"},
+                        {name: "imperium.html"},
+                    ]
+                },
+                {
+                    name: "school/",
+                    subTree: [
+
+                    ]
+                },
+                {
+                    name: "research/",
+                    subTree: [
+
+                    ]
+                }
+            ]
+        };
+        function recurse(tree){
+            if(tree.name.endsWith("/"))
+                return <div className="sidebarItem sidebarFolder w3-row"><img className='folderIcon' alt='folder'/>
+                    <button id={tree.name.substring(0, tree.name.length-1)+"Folder"} className="w3-button lightText" onClick={
+                        () => {
+                            document.getElementById("langStatus").innerText = "";
+                            document.getElementById("encodingStatus").innerText = "";
+                            document.getElementById("linesStatus").innerText = "";
+                            document.getElementById("sizeStatus").innerText = tree.subTree.length+" Files";
+                            document.getElementById("itemStatus").innerText = tree.name;
+                        }
+                    }>{tree.name}</button>
+                    <div id={tree.name.substring(0, tree.name.length-1)+"Content"} className="w3-row sidebarContent">
+                        {
+                            tree.subTree.map(child => {
+                                return recurse(child);
+                            })
+                        }
+                    </div>
+                </div>;
+            else
+                return <div className="sidebarItem w3-row" style={{marginLeft: '10px'}}>
+                    <img className='htmlIcon' alt='html'/>
+                    <button id={tree.name.substring(0, tree.name.indexOf("."))+"File"} className="w3-button lightText" onClick={
+                        () => common.showPage(tree.name.substring(0, tree.name.indexOf(".")))
+                    }>{tree.name}</button>
+                </div>;
+        }
         return (
             <body className="w3-display-container">
                 <header className="menuBar w3-row" style={{top: '0px'}}>
@@ -27,25 +79,12 @@ class Wrapper extends Component {
                         <button id="collapseSidebar" className="w3-button">&#60;</button>
                         <div id="sidebarContent" className="w3-display-container w3-row">
                             <p className="sidebarItem lightText" style={{borderStyle: 'none'}}>Explorer</p>
-                            <div className="sidebarItem sidebarFolder w3-row"><img className='folderIcon' alt='folder'/><button id="publicFolder" className="w3-button lightText">public/</button>
-                                <div id="publicContent" className="w3-row sidebarContent">
-                                    <div className="sidebarItem w3-row" style={{marginLeft: '10px'}}><img className='htmlIcon' alt='html'/><button id="homeFile" className="w3-button lightText">home.html</button></div>
-                                    <div className="sidebarItem sidebarFolder w3-row" style={{marginLeft: '10px'}}><img className='folderIcon' alt='folder'/><button id="personalFolder" className="w3-button lightText">personal/</button>
-                                        <div id="personalContent" className="w3-row sidebarContent">
-                                            <div className="sidebarItem w3-row" style={{marginLeft: '15px'}}><img className='htmlIcon' alt='html'/><button id="simplexFile" className="w3-button lightText">simplex.html</button></div>
-                                            <div className="sidebarItem w3-row" style={{marginLeft: '15px'}}><img className='htmlIcon' alt='html'/><button id="imperiumFile" className="w3-button lightText">imperium.html</button></div>
-                                        </div>
-                                    </div>
-                                    <div className="sidebarItem w3-row" style={{marginLeft: '10px'}}><img className='htmlIcon' alt='html'/><button id="bioFile" className="w3-button lightText">bio.html</button></div>
-                                    <div className="sidebarItem w3-row" style={{marginLeft: '10px'}}><img className='htmlIcon' alt='html'/><button id="helpFile" className="w3-button lightText">help.html</button></div>
-                                </div>
-                            </div>
+                            {recurse(hierarchy)}
                         </div>
                     </div>
                     <Home/>
                     <Simplex/>
                     <Imperium/>
-                    {/*<Custom/>*/}
                     <div id="fileEditor" className="container w3-rest lightText w3-row" style={{display: 'none'}}>
                         <div id="editorLines" className="w3-col">1</div>
                         <div id="editorContent" className="w3-col"contentEditable="true"></div>
@@ -56,6 +95,7 @@ class Wrapper extends Component {
                     <div id="encodingStatus" className="commandItem lightText w3-col" style={{float: 'right'}}>UTF-8</div>
                     <div id="linesStatus" className="commandItem lightText w3-col" style={{float: 'right'}}></div>
                     <div id="sizeStatus" className="commandItem lightText w3-col" style={{float: 'right'}}></div>
+                    <div id="itemStatus" className="commandItem lightText w3-col" style={{float: 'right'}}>home.html</div>
                 </footer>
             </body>
         );
