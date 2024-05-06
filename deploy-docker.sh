84110cf25e741293fac640fcc99bcd16557777af
matthew-pisano <matthewpisano14@gmail.com>
2024-05-06 17:10:14 -0400

    Update Scripts

--- deploy-docker.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-environ=prod
-
-docker build -t portfolio:latest .
-docker export $(docker create portfolio:latest) > build/portfolio.tar
-
-scp -i /home/matthew/auth/LightSail.pem -r ./build/portfolio.tar bitnami@174.129.214.82:/home/bitnami/portfolio/$environ
