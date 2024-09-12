7c9194886317f596b02cdb1a9065fd239aea4989
matthew-pisano <matthewpisano14@gmail.com>
2024-09-11 20:10:58 -0400

    Add Color Theme Template

--- pages/_document.js
+++ pages/_document.js
@@ -9,6 +9,7 @@ export default function Document() {
 
                 <link href="https://cdn.jsdelivr.net/npm/latex.js@0.12.4/dist/css/katex.css" rel="stylesheet" type="text/css"/>
                 <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css"/>
+                <link href="/css/variables.css" rel="stylesheet" type="text/css"/>
                 <link href="/css/common.css" rel="stylesheet" type="text/css"/>
                 <link href="/css/pages.css" rel="stylesheet" type="text/css"/>
                 <link href="/css/terminal.css" rel="stylesheet" type="text/css"/>
