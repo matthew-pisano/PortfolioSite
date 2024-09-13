795d60fd19108ce4ca2b1cec965efb5de56b5f0a
matthew-pisano <matthewpisano14@gmail.com>
2024-09-13 00:40:12 -0400

    Separate out Terminal Event Handlers

---
+++ scripts/terminal/eventHandlers.js
@@ -0,0 +1,196 @@
+import {showDialog} from "../utils";
+import {SysEnv} from "../fileSystem/fileSystemMeta";
+import {masterFileSystem} from "../fileSystem/buildfs";
+
+export class EventHandlers {
+
+    /**
+     * Whether the terminal is being dragged.  Needs to be global to be accessed by the event listeners
+     * @type {boolean}
+     */
+    static isDragging = false;
+
+    /**
+     * The number of file drag enter events
+     * @type {number}
+     */
+    static enterEvents = 0;
+    /**
+     * The number of file drag leave events
+     * @type {number}
+     */
+    static leaveEvents = 0;
+
+    /**
+     * The index of the command in the history
+     * @type {number}
+     */
+    static commandIndex = -1;
+
+
+    /**
+     * The previous commands in the terminal history
+     * @type {[string]}
+     */
+    static prevCommands = [""];
+
+    /**
+     * The draft command in the terminal
+     * @type {string}
+     */
+    static draftCommand = "";
+
+
+    /**
+     * The height of the terminal
+     * @type {number}
+     */
+    static terminalHeight = 0;
+
+    static addEventListeners(resizeTerminal) {
+        // Listeners need to be global to avoid losing track of the thumb
+        document.addEventListener("mousemove", (evt) => this.thumbDrag(evt, resizeTerminal));
+        document.addEventListener("mouseup", this.thumbDragEnd);
+        document.body.addEventListener('drop', (evt) => evt.preventDefault(), false);
+        document.body.addEventListener('dragover', (evt) => evt.preventDefault(), false);
+        // Capture ALT + SHIFT + `
+        document.addEventListener('keydown', e => {
+            if (e.shiftKey && e.altKey && e.key === '`' && !e.shiftKey) {
+                // Prevent the Save dialog to open
+                e.preventDefault();
+                document.getElementById("terminalHeader").click();
+            }
+        });
+    }
+
+    /**
+     * Handles the drag enter event for the terminal
+     * @param e {DragEvent} The drag event
+     */
+    static onDragEnter(e) {
+        this.enterEvents += 1;
+        e.preventDefault();
+
+        if (!e.dataTransfer.items || e.dataTransfer.items.length === 0 || e.dataTransfer.items[0].kind !== "file") return;
+
+        document.getElementById('terminalFileHandler').style.visibility = "visible";
+    }
+
+    /**
+     * Handles the drag leave event for the terminal
+     * @param e {DragEvent} The drag event
+     */
+    static onDragLeave(e) {
+        this.leaveEvents += 1;
+        e.preventDefault();
+
+        if (this.leaveEvents < this.enterEvents) return;
+
+        document.getElementById('terminalFileHandler').style.visibility = "hidden";
+    }
+
+    /**
+     * Handles the drop event for the terminal
+     * @param e {DragEvent} The drag event
+     */
+    static onDrop(e) {
+        this.enterEvents = 0;
+        this.leaveEvents = 0;
+        e.preventDefault();
+
+        let files = e.dataTransfer.files;
+        for (let file of files) {
+
+            if (!file.type.startsWith("text")) {
+                showDialog("Invalid File", `'${file.name}' is not a text file`);
+                continue;
+            }
+
+            let reader = new FileReader();
+            reader.onload = (e) => {
+                try {
+                    let text = e.target.result;
+
+                    let mntPath = SysEnv.MOUNT_FOLDER + "/" + file.name;
+                    masterFileSystem.writeText(mntPath, text);
+
+                    showDialog("Copied File", `Copied to '${mntPath}'`);
+                } catch (e) {
+                    showDialog("Error", e.message);
+                }
+            };
+            reader.readAsText(file);
+        }
+
+        document.getElementById('terminalFileHandler').style.visibility = "hidden";
+    }
+
+    /**
+     * Initiates the terminal thumb drag
+     */
+    static thumbDragStart() {
+        EventHandlers.isDragging = true;
+    }
+
+    /**
+     * Handles the drag event for the terminal thumb
+     * @param e {MouseEvent} The mouse move event
+     * @param resizeTerminal {function} The function to resize the terminal
+     */
+    static thumbDrag(e, resizeTerminal) {
+        if (!this.isDragging) return;
+        let thumb = document.getElementById('terminalThumb');
+        let newHeight = thumb.getBoundingClientRect().top - e.clientY + this.terminalHeight;
+        resizeTerminal(newHeight);
+    }
+
+    /**
+     * Terminates the terminal thumb drag
+     */
+    static thumbDragEnd() {
+        EventHandlers.isDragging = false;
+    }
+
+    /**
+     * Handles the key down event
+     * @param e {object} The key down event
+     * @param submit {function} The function to submit the command
+     */
+    static onKeyDown(e, submit) {
+        let terminalInput = document.getElementById('terminalInput');
+        // Scroll the terminal output to the bottom on input
+        let terminalOutput = document.getElementById('terminalOutput');
+        terminalOutput.scrollTop = terminalOutput.scrollHeight;
+
+        if (e.code === "Enter") {
+            e.preventDefault();
+            submit();
+        }
+        // Handle the arrow keys to navigate the command history
+        else if (e.code === "ArrowUp") {
+            // Save the draft command if it is not in the history
+            if (this.commandIndex === -1 && terminalInput.innerText.length > 0)
+                this.draftCommand = terminalInput.innerText.replace("\n", "").replace("\r", "");
+
+            if (this.commandIndex < this.prevCommands.length - 1) this.commandIndex++;
+            terminalInput.innerText = this.prevCommands[this.prevCommands.length - 1 - this.commandIndex];
+        } else if (e.code === "ArrowDown") {
+            if (this.commandIndex > -1) this.commandIndex--;
+            terminalInput.innerText = this.commandIndex > -1 ? this.prevCommands[this.prevCommands.length - 1 - this.commandIndex] : this.draftCommand;
+        }
+    }
+
+    /**
+     * Adds the command to the terminal history
+     * @param command {string} The command to add
+     */
+    static submitCommand(command) {
+        // Add the command to the history
+        if (this.prevCommands[0] === "" && command.length > 0)
+            this.prevCommands = ([command, ...this.prevCommands.slice(1)]);
+        else if (this.prevCommands[this.prevCommands.length - 1] !== command && command.length > 0)
+            this.prevCommands = [...this.prevCommands, command];
+        this.draftCommand = "";
+        this.commandIndex = -1;
+    }
+}
--- scripts/terminal/terminal.js
+++ scripts/terminal/terminal.js
@@ -1,16 +1,8 @@
 import $ from 'jquery';
 import React, {useEffect, useState} from 'react';
-import {showDialog} from '../utils';
 import {Commands} from './commands';
-import {masterFileSystem} from "../fileSystem/buildfs";
 import {ANSI, SysEnv} from "../fileSystem/fileSystemMeta";
-
-
-/**
- * Whether the terminal is being dragged.  Needs to be global to be accessed by the event listeners
- * @type {boolean}
- */
-let isDragging = false;
+import {EventHandlers} from "./eventHandlers";
 
 
 /**
@@ -19,27 +11,7 @@ let isDragging = false;
  */
 function TerminalDiv() {
 
-    const [prevCommands, setPrevCommands] = useState([""]);
-    const [draftCommand, setDraftCommand] = useState("");
-    const [commandIndex, setCommandIndex] = useState(-1);
     const [ENV, setENV] = useState({ CWD: SysEnv.HOME_FOLDER, HOME: SysEnv.HOME_FOLDER, COLOR: "#ffffff", CLOSED: true, CLOSE_TIME: 0 });
-
-    /**
-     * The height of the terminal
-     * @type {number}
-     */
-    let terminalHeight = 0;
-
-    /**
-     * The number of file drag enter events
-     * @type {number}
-     */
-    let enterEvents = 0;
-    /**
-     * The number of file drag leave events
-     * @type {number}
-     */
-    let leaveEvents = 0;
 
     /**
      * Generates the prompt string
@@ -67,9 +39,7 @@ function TerminalDiv() {
             resize(evt.detail ? evt.detail: 210);
         });
 
-        document.getElementById("terminal").addEventListener("close", () => {
-            exit();
-        });
+        document.getElementById("terminal").addEventListener("close", exit);
 
         // Add zoom functionality to the terminal
         $("#terminal").bind('mousewheel DOMMouseScroll', (evt) => {
@@ -83,24 +53,8 @@ function TerminalDiv() {
             }
         });
 
-        document.getElementById("terminalInput").addEventListener("submit", () => {
-            submit();
-        });
-
         setPrompt(genPrompt(ENV.CWD, true));
-        // Listeners need to be global to avoid losing track of the thumb
-        document.addEventListener("mousemove", thumbDrag);
-        document.addEventListener("mouseup", thumbDragEnd);
-        document.body.addEventListener('drop', (evt) => evt.preventDefault(), false);
-        document.body.addEventListener('dragover', (evt) => evt.preventDefault(), false);
-        // Capture ALT + SHIFT + `
-        document.addEventListener('keydown', e => {
-            if (e.shiftKey && e.altKey && e.key === '`' && !e.shiftKey) {
-                // Prevent the Save dialog to open
-                e.preventDefault();
-                document.getElementById("terminalHeader").click();
-            }
-        });
+        EventHandlers.addEventListeners(resize);
     }, []);
 
     useEffect(() => {
@@ -112,31 +66,6 @@ function TerminalDiv() {
         terminalHolder.style.color = ENV.COLOR;
         document.getElementById("terminalPrompt").innerHTML = newPrompt;
     }, [ENV]);
-
-    /**
-     * Initiates the terminal thumb drag
-     */
-    function thumbDragStart() {
-        isDragging = true;
-    }
-
-    /**
-     * Handles the drag event for the terminal thumb
-     * @param e {MouseEvent} The mouse move event
-     */
-    function thumbDrag(e) {
-        if (!isDragging) return;
-        let thumb = document.getElementById('terminalThumb');
-        let newHeight = thumb.getBoundingClientRect().top - e.clientY + terminalHeight;
-        resize(newHeight);
-    }
-
-    /**
-     * Terminates the terminal thumb drag
-     */
-    function thumbDragEnd() {
-        isDragging = false;
-    }
 
     /**
      * Resizes the terminal to the given height
@@ -161,7 +90,7 @@ function TerminalDiv() {
         document.getElementById('terminalClose').style.visibility = "visible";
         ENV.CLOSED = false;
         setENV(ENV);
-        terminalHeight = height;
+        EventHandlers.terminalHeight = height;
     }
 
     /**
@@ -181,11 +110,7 @@ function TerminalDiv() {
         terminalBottom.style.visibility = "hidden";
 
         // Add the command to the history
-        if (prevCommands[0] === "" && command.length > 0) setPrevCommands([command, ...prevCommands.slice(1)]);
-        else if (prevCommands[prevCommands.length - 1] !== command && command.length > 0) setPrevCommands([...prevCommands, command]);
-
-        setDraftCommand("");
-        setCommandIndex(-1);
+        EventHandlers.submitCommand(command);
 
         if (terminalOutput.innerHTML && !terminalOutput.innerHTML.replaceAll("</span>", "").endsWith("<br>"))
             terminalOutput.innerHTML += "<br>";
@@ -221,110 +146,6 @@ function TerminalDiv() {
     }
 
     /**
-     * Handles the input event
-     * @param e {object} The input event
-     */
-    function onInput(e) {
-        // Submit the command if the user presses enter
-        if (e.nativeEvent.inputType === "insertParagraph" || e.nativeEvent.data === null && e.nativeEvent.inputType === "insertText")
-            submit();
-
-        // Scroll the terminal output to the bottom on input
-        let terminalOutput = document.getElementById('terminalOutput');
-        terminalOutput.scrollTop = terminalOutput.scrollHeight;
-    }
-
-    /**
-     * Handles the key down event
-     * @param e {object} The key down event
-     */
-    function onKeyDown(e) {
-        let newI = commandIndex;
-        let terminalInput = document.getElementById('terminalInput');
-        // Handle the arrow keys to navigate the command history
-        if (e.code === "ArrowUp") {
-            // Save the draft command if it is not in the history
-            if (commandIndex === -1 && terminalInput.innerText.length > 0)
-                setDraftCommand(terminalInput.innerText.replace("\n", "").replace("\r", ""));
-
-            if (commandIndex < prevCommands.length - 1) {
-                setCommandIndex(commandIndex + 1);
-                newI++;
-            }
-            terminalInput.innerText = prevCommands[prevCommands.length - 1 - newI];
-        }
-        else if (e.code === "ArrowDown") {
-            if (commandIndex > -1) {
-                setCommandIndex(commandIndex - 1);
-                newI--;
-            }
-            terminalInput.innerText = newI > -1 ? prevCommands[prevCommands.length - 1 - newI] : draftCommand;
-        }
-    }
-
-    /**
-     * Handles the drag enter event for the terminal
-     * @param e {DragEvent} The drag event
-     */
-    function onDragEnter(e) {
-        enterEvents += 1;
-        e.preventDefault();
-
-        if (!e.dataTransfer.items || e.dataTransfer.items.length === 0 || e.dataTransfer.items[0].kind !== "file") return;
-
-        document.getElementById('terminalFileHandler').style.visibility = "visible";
-    }
-
-    /**
-     * Handles the drag leave event for the terminal
-     * @param e {DragEvent} The drag event
-     */
-    function onDragLeave(e) {
-        leaveEvents += 1;
-        e.preventDefault();
-
-        if (leaveEvents < enterEvents) return;
-
-        document.getElementById('terminalFileHandler').style.visibility = "hidden";
-    }
-
-    /**
-     * Handles the drop event for the terminal
-     * @param e {DragEvent} The drag event
-     */
-    function onDrop(e) {
-        enterEvents = 0;
-        leaveEvents = 0;
-        e.preventDefault();
-
-        let files = e.dataTransfer.files;
-        for (let file of files) {
-
-            if (!file.type.startsWith("text")) {
-                showDialog("Invalid File", `'${file.name}' is not a text file`);
-                continue;
-            }
-
-            let reader = new FileReader();
-            reader.onload = (e) => {
-                try {
-                    let text = e.target.result;
-
-                    let mntPath = SysEnv.MOUNT_FOLDER + "/" + file.name;
-                    masterFileSystem.writeText(mntPath, text);
-
-                    showDialog("Copied File", `Copied to '${mntPath}'`);
-                }catch (e) {
-                    showDialog("Error", e.message);
-                }
-            };
-            reader.readAsText(file);
-        }
-
-        document.getElementById('terminalFileHandler').style.visibility = "hidden";
-    }
-
-    /**
      * Exits the terminal
      */
     async function exit() {
@@ -338,7 +159,7 @@ function TerminalDiv() {
             <div id='terminalFileHandler'>
                 <span id="terminalFileIndicator">Drag Files Here</span>
             </div>
-            <div id='terminalThumb' onMouseDown={thumbDragStart}><span id='terminalThumbDots'>• • •</span></div>
+            <div id='terminalThumb' onMouseDown={EventHandlers.thumbDragStart}><span id='terminalThumbDots'>• • •</span></div>
             <div id="terminalHeader" onClick={() => {
                 if (ENV.CLOSED && Date.now() - ENV.CLOSE_TIME > 500) resize(210);
             }}>
@@ -350,11 +171,12 @@ function TerminalDiv() {
                 // Ensure the user is not selecting text, and it is a single click
                 if (e.detail === 1 && window.getSelection().isCollapsed)
                     document.getElementById('terminalInput').focus();
-            }} onDragEnter={onDragEnter} onDragLeave={onDragLeave} onDrop={onDrop}>
+            }} onDragEnter={EventHandlers.onDragEnter} onDragLeave={EventHandlers.onDragLeave} onDrop={EventHandlers.onDrop}>
                 <div id="terminalOutput"></div>
                 <div id="terminalBottom">
                     <div id="terminalPrompt"></div>
-                    <div id="terminalInput" contentEditable="true" onInput={onInput} onKeyDown={onKeyDown}></div>
+                    <div id="terminalInput" contentEditable="true"
+                         onKeyDown={(e) => EventHandlers.onKeyDown(e, submit)}></div>
                 </div>
             </div>
         </div>
