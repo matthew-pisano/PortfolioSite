8142121e84c42024289a9533467d151fc8ed4901
matthew-pisano <matthewpisano14@gmail.com>
2024-10-18 20:46:14 -0400

    Simplify Buildfs Logic

--- scripts/fileSystem/buildfs.js
+++ scripts/fileSystem/buildfs.js
@@ -1,4 +1,3 @@
-import {Directory} from "./fileSystemObjects";
 import {FileSystem} from "./fileSystem";
 import {bootstrapServerside} from "./bootstrap";
 
@@ -11,19 +10,6 @@ let masterFileSystem;
 
 
 /**
- * Sets the master file system to a new file system
- * @param hierarchyDict {object} The dictionary representation of the new hierarchy
- */
-function setMasterFileSystem(hierarchyDict) {
-    console.log("Setting master file system", hierarchyDict);
-    let newMaster = new FileSystem(Directory.deserialize(hierarchyDict));
-    if (masterFileSystem !== undefined) newMaster.callbacks = masterFileSystem.callbacks;
-    masterFileSystem = newMaster;
-    masterFileSystem.update();
-}
-
-
-/**
  * Builds the file system on the client side by rehydrating the page registry and the master file system
  * @returns {string} The dehydrated info
  */
@@ -32,7 +18,7 @@ function buildClientside() {
     // The dehydrated info is stored in a hidden element on the page
     let savedHierarchy = localStorage.getItem("hierarchy");
     let dehydratedInfo = savedHierarchy ? savedHierarchy : dehydrateElem.innerText;
-    setMasterFileSystem(JSON.parse(dehydratedInfo).hierarchy);
+    masterFileSystem = FileSystem.deserialize(JSON.parse(dehydratedInfo));
     return dehydrateElem.innerText;
 }
 
@@ -42,10 +28,9 @@ function buildClientside() {
  * @returns {string} The dehydrated info
  */
 function buildServerside() {
-    let serverSideFS = bootstrapServerside();
-    setMasterFileSystem(serverSideFS.hierarchy.serialize());
+    masterFileSystem = bootstrapServerside();
     // Create the dehydrated info from the server file system and page registry
-    return JSON.stringify(serverSideFS.serialize());
+    return JSON.stringify(masterFileSystem.serialize());
 }
 
 
