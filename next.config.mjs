adc2666ce12072f6f9251cca50e8db8c39d14536
matthew-pisano <matthewpisano14@gmail.com>
2025-03-10 23:36:42 -0400

    Add Basic Security Headers

--- next.config.mjs
+++ next.config.mjs
@@ -15,6 +15,39 @@ const nextConfig = {
             },
         ];
     },
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'same-origin',
+                    },
+                    {
+                        key: 'Strict-Transport-Security',
+                        value: 'max-age=31536000; includeSubDomains; preload',
+                    },
+                    {
+                        key: 'Permissions-Policy',
+                        value: '',
+                    },
+                    {
+                        key: 'X-XSS-Protection',
+                        value: '1; mode=block',
+                    },
+                ],
+            },
+        ];
+    },
 };
    
 export default nextConfig;
