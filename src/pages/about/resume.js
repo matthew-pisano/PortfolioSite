62e7a5b7615648a81b99271eeedb3be6452cca0e
matthew-pisano <matthewpisano14@gmail.com>
2025-03-30 19:36:16 -0400

    Fix Positioning of Resume Page

--- src/pages/about/resume.js
+++ src/pages/about/resume.js
@@ -3,6 +3,7 @@ import React from 'react';
 import Wrapper from '@/components/Wrapper';
 import {PageInfo} from "@/lib/pageBuilder";
 import {Constants} from '@/lib/utils';
+import styles from '@/styles/pageTiles.module.css';
 
 
 export default function Resume() {
@@ -13,9 +14,8 @@ export default function Resume() {
         "A brief overview of my resume",
     );
 
-    let style = {borderRadius: "5px", "position": "fixed", top: "100px", left: "0", "bottom": "0", right: "0"};
-    return (<Wrapper pageName={pageInfo.pageName} pageStyle={style}>
-        <embed id="resumeEmbed" style={{width: "100%", height: "100%"}}
+    return (<Wrapper pageName={pageInfo.pageName} pageClass={styles.resumePage}>
+        <embed id="resumeEmbed" style={{width: "100%", height: "100%", borderRadius: "10px"}}
                type="application/pdf" src={Constants.resumeUrl+"#zoom=FitH"}/>
     </Wrapper>);
 }
