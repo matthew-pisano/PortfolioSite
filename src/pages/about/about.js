ebb0dab96b9a577d7b6755be21ba7e15df751c71
matthew-pisano <matthewpisano14@gmail.com>
2025-04-06 14:51:14 -0400

    Update Skills, Teaching Experience, and Leadership Sections

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -13,61 +13,97 @@ export default function About() {
     let tiles = [
         new Tile(
             <h2>Introduction</h2>,
-            <>My name is Matthew Pisano.  I am an Artificial Intelligence researcher and
-                software developer.  I have a Master's degree in Computer Science from Rensselaer
-                Polytechnic Institute.
+            <>My name is Matthew Pisano.  I am an Artificial Intelligence Researcher and
+                Software Engineer.  I have a Master's degree in Computer Science from Rensselaer
+                Polytechnic Institute along with extensive work experience in the domains of
+                artificial intelligence, academia, and software development.
                 <br/><br/>
                 My research centers around language model alignment; enforcing safety without
                 hampering its cognitive abilities. I often draw inspiration from biological intelligences
                 to aid im my work with artificial ones.  I enjoy learning about how the mind works
-                and the cognitive principles that can be applied to it.</>,
+                and the cognitive principles that it operates upon.</>,
             "/media/image/headshot"
         ),
         new Tile(
             <>Software Skills</>,
-            <>While I have experience in many languages, my skills in <code>Python</code>
-                , <code>C++</code>, <code>JavaScript</code>, and <code>Java</code> are the most developed.
+            <>While I have experience in many languages, my skills in <i>Python</i>
+                , <i>C++</i>, <i>JavaScript</i>, and <i>Java</i> are the most developed.
                 <ul>
-                    <li>I have created many research and enterprise machine learning applications, utilizing
-                        libraries such as those from Hugging Face, along with PyTorch.</li>
-                    <li>I have used javascript in a variety of personal and professional web applications,
-                        with React.js and Node.js being the libraries in which I have the most experience.</li>
-                    <li>Through Java, I have created multiple android applications, some of which have been
-                        previously published on the Google Play Store.</li>
-                </ul></>,
-        ),
-        new Tile(
-            <>Development Experience</>,
-            <>Through both my industry experience and my higher education, I have gained invaluable
-                experience as a developer.  I have experience using the DevOps and Agile methodologies,
-                full-stack development, and continuous integration and continuous delivery using AWS.
-                <br/><br/>
+                    <li>I have created several production-ready research and enterprise machine learning
+                        applications, utilizing libraries such as HuggingFace, PyTorch, and CUDA.</li>
+                    <li>Through Python, I have developed novel research into LLM alignment, worked
+                        with the low-level C++ bindings of PyTorch, and developed a distributed and
+                        asynchronous cloud-to-cloud backup SaaS.</li>
+                    <li>With C and C++, I have taught students about data structures, developed
+                        highly parallelized population simulations, created byte-encoding CLI utilities,
+                        and worked on AI model compilation to enterprise grade accelerator cards.</li>
+                    <li>In the more meta domain of DevOps, I have extensive experience
+                        directing small teams, working with CI/CD pipelines, using project
+                        management software, and mentoring others in best-practice testing and
+                        clean code standards.</li>
+                </ul>
                 I have work experience ranging from small startups to Multinational Research divisions.
                 With each new experience, I gain valuable knowledge on the unique cultures and
-                strategies that they offer.</>,
+                strategies that they offer.
+            </>,
+        ),
+        new Tile(
+            <>Teaching Experience</>,
+            <>Throughout my education and career, I have grained experience teaching at several levels.
+                I have worked as a student mentor, graduate teaching assistant, and now as an adjunct professor.
+                One of my passions is to be able to share the knowledge I have with others and an
+                effective manner.  For me, teaching and lecturing fulfills this goal.
+
+                <p>Most recently, I have worked at SUNY Ulster as a computer science adjunct for
+                    their <i>Computer Architecture and Organization</i> class.  Here, I teach students
+                    about the MIPS assembly language and its corresponding processor architecture.
+                    In my class, we also explore basic CPU architectures, memory hierarchies,
+                    parallelism, virtual machines, and many other topics in this domain.</p>
+
+                <p>At RPI, I worked as a teaching assistant for
+                    their <i>Principles of Software</i> and <i>Data Structures</i> classes.  I interacted
+                    with students at an individual level, held office hours, graded assignments, and
+                    participated in the rubric creation process for exams.</p>
+
+                <p>As an undergraduate student at SUNY Ulster, I worked as a student mentor for
+                their math program.  I aided other students in areas ranging from pre-algebra to
+                calculus and differential equations.</p></>,
         ),
         new Tile(
             <>Leadership</>,
             <>A key attribute of someone who can work well with a team, whether the focus is
-                software development or research, is leadership.  Many of my leadership skills
-                come from my participation in research projects and my time in scouting.
-                <br/><br/>
-                I have worked on numerous different academic and research projects.  In many
-                of these, I was the primary organizer of the project and in charge of delegating
-                tasks to collaborators or teammates.  This is evident in my projects
-                like <Link href="/research/predictChain">Predict Chain</Link>
-                , <Link href="/hackathons/anonHires">Anonymous Hiring</Link>, or my Master's
-                thesis, <Link href="/research/bergeron">Bergeron</Link>.
-                <br/><br/>
-                I spent 10 years in the Boy Scouts of America program learning the attributes
-                of good leaders and good team players.  This culminated in my Eagle Scout project where
-                I preformed renovations on my community's local recreation center.  My experience has
-                continued on from there as I took active roles in the planning and machine learning
-                components of <i>PredictChain</i>, the cellular automata CUDA algorithm and the
-                population growth calculations within <i>Manifest Destiny</i>, the front-end and ChatGPT
-                interface of <Link href="https://github.com/GPT-Pedagogy/GPT-Pedagogy" target="_blank">Mathesis</Link>,
-                leading the front-end development of the <i>Anonymous Hires</i> project, and in
-                SUNY Ulster's Team Orion collaboration with IBM.</>,
+                software development or research, is leadership.  My own leadership skills
+                stem from my experience in leading development teams, my participation in research
+                endeavours, and my many group projects throughout my education.
+
+                <p>While on IBM's Spyre development team, I was placed in charge of managing our
+                    team's sprints, creating and distributing tasks, and helping other team members
+                    uphold IBM's high standards for testing and clean code.  At Substrate, I was the
+                    main contributor to our LLM development.  Here, I helped to direct members from their
+                    dataset and deployment teams on how to best facilitate the development of
+                    our <i>Metacontrol</i> model.  At FileScience, my skills in testing and QA lead
+                    to my promotion as the team's lead for quality assurance.  In this role, I
+                    worked autonomously to critique our current development practices and lead our
+                    team to a more readable and maintainable style of programming.</p>
+
+                <p>I have also worked on numerous different academic and research projects.  In many
+                    of these, I was the primary organizer of the project and in charge of delegating
+                    tasks to collaborators or teammates.  This is evident in my projects
+                    like <Link href="/research/highGround">Moral High Ground</Link> where I worked with
+                    a team from IBM research to develop a conversational alignment
+                    benchmark, <Link href="/research/predictChain">Predict Chain</Link> where I lead
+                    our small team to winning first place globally in the Mega-Ace Algorand
+                    hackathon, <Link href="/school/destiny">Manifest Destiny</Link> in which I was in
+                    charge of ideation and the development of our population growth algorithm, or my
+                    Master's thesis: <Link href="/research/bergeron">Bergeron</Link>.</p>
+
+                <p>I spent 10 years in the Boy Scouts of America program learning the attributes
+                    of good leaders and good team players.  This culminated in my Eagle Scout project
+                    where I preformed renovations on my community's local recreation center.  This
+                    was one of my first real experiences to lead a group along with my term as my
+                    troop's Senior Patrol Leader.  While my leadership in these settings did not have
+                    the same scope as my later projects, it helped to set the ground work for my
+                    current proactive and collaborative approach to leadership.</p></>,
         ),
         new SectionTile(
             <>Employment History</>,
