f3cb17bca91f3964f0a390802bcc426acd116628
matthew-pisano <matthewpisano14@gmail.com>
2025-04-06 13:24:08 -0400

    Update Work Experience Tiles and Add Company Logos

--- src/pages/about/about.js
+++ src/pages/about/about.js
@@ -4,7 +4,7 @@ import Link from "next/link";
 
 
 import DefaultWrapper from "@/components/DefaultWrapper";
-import {GitLink, PageInfo, Tile, TileLink} from '@/lib/pageBuilder';
+import {GitLink, PageInfo, SectionTile, Tile, TileLink} from '@/lib/pageBuilder';
 import {Constants, genPageTitle} from "@/lib/utils";
 
 
@@ -14,14 +14,14 @@ export default function About() {
         new Tile(
             <h2>Introduction</h2>,
             <>My name is Matthew Pisano.  I am an Artificial Intelligence researcher and
-                software developer.  I have a Master's degree in Computer Science from Rensselaer 
+                software developer.  I have a Master's degree in Computer Science from Rensselaer
                 Polytechnic Institute.
                 <br/><br/>
                 My research centers around language model alignment; enforcing safety without
-                hampering its cognitive abilities. I often draw inspiration from biological intelligences 
-                to aid im my work with artificial ones.  I enjoy learning about how the mind works 
+                hampering its cognitive abilities. I often draw inspiration from biological intelligences
+                to aid im my work with artificial ones.  I enjoy learning about how the mind works
                 and the cognitive principles that can be applied to it.</>,
-             "/media/image/headshot"
+            "/media/image/headshot"
         ),
         new Tile(
             <>Software Skills</>,
@@ -29,11 +29,11 @@ export default function About() {
                 , <code>C++</code>, <code>JavaScript</code>, and <code>Java</code> are the most developed.
                 <ul>
                     <li>I have created many research and enterprise machine learning applications, utilizing
-                    libraries such as those from Hugging Face, along with PyTorch.</li>
-                    <li>I have used javascript in a variety of personal and professional web applications, 
-                    with React.js and Node.js being the libraries in which I have the most experience.</li>
-                    <li>Through Java, I have created multiple android applications, some of which have been 
-                    previously published on the Google Play Store.</li>
+                        libraries such as those from Hugging Face, along with PyTorch.</li>
+                    <li>I have used javascript in a variety of personal and professional web applications,
+                        with React.js and Node.js being the libraries in which I have the most experience.</li>
+                    <li>Through Java, I have created multiple android applications, some of which have been
+                        previously published on the Google Play Store.</li>
                 </ul></>,
         ),
         new Tile(
@@ -42,68 +42,9 @@ export default function About() {
                 experience as a developer.  I have experience using the DevOps and Agile methodologies,
                 full-stack development, and continuous integration and continuous delivery using AWS.
                 <br/><br/>
-                I have work experience ranging from small startups to Multinational Research divisions.  
-                With each new experience, I gain valuable knowledge on the unique cultures and 
+                I have work experience ranging from small startups to Multinational Research divisions.
+                With each new experience, I gain valuable knowledge on the unique cultures and
                 strategies that they offer.</>,
-        ),
-        new Tile(
-            <>Recent Employment</>,
-            <>
-                <p><i>IBM</i></p>
-                As a part of IBM's AI Enablement department, I have worked on low-level C and C++
-                development for compiling PyTorch models onto IBM's new <i>Sypre</i> AI accelerator
-                chips.  I have also been tasked with automating our build workflows and organize
-                our team scrums and pull-request meetings.
-
-                <p><i>SUNY Ulster</i></p>
-                Since the Spring semester of 2025, I have been an Adjunct Professor at SUNY Ulster.
-                Here, I teach the college's course on assembly language and computer architecture.
-                My responsibilities include lecturing to the class, managing the course curriculum
-                and creating new modules as needed, grading student work, and participating in
-                faculty meetings.
-
-                <p><i>Substrate AI Research</i></p>
-                During the second half of 2024, I worked on the development of a <i>Metacontrol</i> large
-                language model that could evaluate whether user queries violated a given set of policies,
-                queries were then passed to a more generalized assistant. Here, I used <i>LoRA</i> fine-tuning and
-                dataset cleaning to generate high-quality results from sparse, synthetically generated data.
-                On this team, I was the primary contributor towards the training of the model and the
-                development of the supporting infrastructure for production deployment.
-                
-                <p><i>FileScience</i></p>
-                As a founding member of the <Link href="https://filescience.io/" target="_blank">FileScience</Link> team,
-                I have personally worked with small team to develop a seamless and reliable
-                Cloud-to-Cloud backup service.  As this is geared towards enterprise clients, one of 
-                most important roles is to ensure that the code we produce is testable, maintainable,
-                and fault-tolerant.  This has given me invaluable experience with architecting highly 
-                distributed and parallelized software and infrastructure.
-
-                <p><i>IBM Research</i></p>
-                Over the course of Summer 2023, I worked as a Visiting Researcher at IBM's research
-                facility in Yorktown as part of their extern program.  During this time, I researched
-                new techniques of model alignment using text-based game for safety training on
-                moral and ethical principles.  More information can be found in
-                the <Link href="/research/highGround">Moral High Ground</Link> page.
-                
-                <p><i>Cyber Guardian Consulting Group</i></p>
-                Through my employment at <Link href="https://cgcg.biz/" target="_blank">Cyber Guardian Consulting Group</Link>, I
-                have developed a variety of proprietary business solutions using a variety of languages
-                and web services, primarily AWS.  These often involved custom SaaS solutions for clients, 
-                writing distributed and asynchronous backup software from scratch, database management, 
-                and many full-stack web applications.
-                
-                <p><i>Rensselaer Polytechnic Institute</i></p>
-                Throughout my graduate education, I had been employed at Rensselaer Polytechnic 
-                Institute as a teaching assistant.  In this position, I hold office hours, aid the 
-                professor in grading, and help students to better grasp relevant course material.
-                
-                <p><i>SUNY New Paltz</i></p>
-                During the Summer of 2022, I had worked for SUNY New Paltz through their summer 
-                undergraduate research experience, details about this can be found in
-                the <Link href="/research/chipFiring">Chip Firing</Link> page and in
-                the <Link href="https://arxiv.org/abs/2302.10327" target="_blank">arXiv submission</Link> for
-                the resulting paper.  With this job, I worked closely with my professor to gain 
-                research experience and to develop research for later publishing.</>,
         ),
         new Tile(
             <>Leadership</>,
@@ -112,21 +53,106 @@ export default function About() {
                 come from my participation in research projects and my time in scouting.
                 <br/><br/>
                 I have worked on numerous different academic and research projects.  In many
-                of these, I was the primary organizer of the project and in charge of delegating 
+                of these, I was the primary organizer of the project and in charge of delegating
                 tasks to collaborators or teammates.  This is evident in my projects
                 like <Link href="/research/predictChain">Predict Chain</Link>
                 , <Link href="/hackathons/anonHires">Anonymous Hiring</Link>, or my Master's
                 thesis, <Link href="/research/bergeron">Bergeron</Link>.
                 <br/><br/>
                 I spent 10 years in the Boy Scouts of America program learning the attributes
-                of good leaders and good team players.  This culminated in my Eagle Scout project where 
-                I preformed renovations on my community's local recreation center.  My experience has 
-                continued on from there as I took active roles in the planning and machine learning 
-                components of <i>PredictChain</i>, the cellular automata CUDA algorithm and the 
-                population growth calculations within <i>Manifest Destiny</i>, the front-end and ChatGPT 
+                of good leaders and good team players.  This culminated in my Eagle Scout project where
+                I preformed renovations on my community's local recreation center.  My experience has
+                continued on from there as I took active roles in the planning and machine learning
+                components of <i>PredictChain</i>, the cellular automata CUDA algorithm and the
+                population growth calculations within <i>Manifest Destiny</i>, the front-end and ChatGPT
                 interface of <Link href="https://github.com/GPT-Pedagogy/GPT-Pedagogy" target="_blank">Mathesis</Link>,
-                leading the front-end development of the <i>Anonymous Hires</i> project, and in 
+                leading the front-end development of the <i>Anonymous Hires</i> project, and in
                 SUNY Ulster's Team Orion collaboration with IBM.</>,
+        ),
+        new SectionTile(
+            <>Employment History</>,
+        ),
+        new Tile(
+            <>IBM</>,
+            <><p><i>Staff Software Engineer</i></p>
+                As a part of IBM's AI Enablement department, I have worked on low-level C and C++
+                development for compiling PyTorch models onto IBM's new <i>Sypre</i> AI accelerator
+                chips.  I have also been tasked with automating our build workflows and organize
+                our team scrums and pull-request meetings.</>,
+            "https://pngimg.com/uploads/ibm/ibm_PNG19658.png"
+        ),
+        new Tile(
+            <>SUNY Ulster</>,
+            <><p><i>Computer Science Adjunct</i></p>
+                Since the Spring semester of 2025, I have worked as a faculty member at SUNY Ulster.
+                Here, I teach the college's course on assembly language and computer architecture.
+                My responsibilities include lecturing to the class, managing the course curriculum
+                and creating new modules as needed, grading student work, and participating in
+                faculty meetings.</>,
+            "https://acctsearches.org/wp-content/uploads/2022/01/suny-ulster-vector-logo.png"
+        ),
+        new Tile(
+            <>Substrate AI Research</>,
+            <><p><i>Research Engineer</i></p>
+                During the second half of 2024, I worked on the development of a <i>Metacontrol</i> large
+                language model that could evaluate whether user queries violated a given set of policies,
+                queries were then passed to a more generalized assistant. Here, I used <i>LoRA</i> fine-tuning and
+                dataset cleaning to generate high-quality results from sparse, synthetically generated data.
+                On this team, I was the primary contributor towards the training of the model and the
+                development of the supporting infrastructure for production deployment.</>,
+            "https://substrate.ai/wp-content/uploads/2022/03/logo_substrate-300x136.png"
+        ),
+        new Tile(
+            <>FileScience</>,
+            <><p><i>Quality Assurance Lead Engineer</i></p>
+                As a founding member of the <Link href="https://filescience.io/" target="_blank">FileScience</Link> team,
+                I had personally worked with a small team to develop a seamless and reliable
+                Cloud-to-Cloud backup service.  As this is geared towards enterprise clients, one of
+                most important goals is to ensure that the code we produce is testable, maintainable,
+                fault-tolerant, and secure.  I took the lead in this regard as I worked to train our small
+                team in industry standard testing, development, and security practices.  This has given me
+                invaluable experience in testing and architecting highly distributed and parallelized
+                software and infrastructure.</>,
+            "https://i.tracxn.com/logo/company/filescience.io_Logo_216e1de5-dc1f-488f-84d5-33f8ad000ec5.jpg"
+        ),
+        new Tile(
+            <>IBM Research</>,
+            <><p><i>Research Extern</i></p>
+                Over the course of Summer 2023, I worked as a Visiting Researcher at IBM's research
+                facility in Yorktown as part of their extern program.  During this time, I researched
+                new techniques of model alignment using text-based game for safety training on
+                moral and ethical principles.  More information can be found in
+                the <Link href="/research/highGround">Moral High Ground</Link> page.</>,
+            "https://avatars.githubusercontent.com/u/22341564?s=280&v=4"
+        ),
+        new Tile(
+            <>Rensselaer Polytechnic Institute</>,
+            <><p><i>Computer Science Teaching Assistant</i></p>
+                Throughout my graduate education, I had been employed at Rensselaer Polytechnic
+                Institute as a teaching assistant.  In this position, I hold office hours, aid the
+                professor in grading, and help students to better grasp relevant course material.</>,
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQwtnWmIefQkU9X48KffqNhfHm8-UWQZzkm4g&s"
+        ),
+        new Tile(
+            <>SUNY New Paltz</>,
+            <><p><i>Undergraduate Researcher</i></p>
+                During the Summer of 2022, I had worked for SUNY New Paltz through their summer
+                undergraduate research experience, details about this can be found in
+                the <Link href="/research/chipFiring">Chip Firing</Link> page and in
+                the <Link href="https://arxiv.org/abs/2302.10327" target="_blank">arXiv submission</Link> for
+                the resulting paper.  With this job, I worked closely with my professor to gain
+                research experience and to develop research for later publishing.</>,
+            "https://www.newpaltz.edu/media/ocm/toolkit/the-new-paltz-brand/vdp_pg13-2415x873.jpg"
+        ),
+        new Tile(
+            <>Cyber Guardian Consulting Group</>,
+            <><p><i>Software Engineer</i></p>
+                Through my employment at <Link href="https://cgcg.biz/" target="_blank">Cyber Guardian Consulting Group</Link>, I
+                have developed a variety of proprietary business solutions using a wide range of languages
+                and web services, primarily AWS.  These often involved custom SaaS solutions for clients,
+                writing distributed and asynchronous backup software from scratch, database management,
+                and many full-stack web applications.</>,
+            "https://media.licdn.com/dms/image/v2/C4E0BAQE4AdTogksURg/company-logo_200_200/company-logo_200_200/0/1643488532634/cyber_guardian_consulting_group_llc__logo?e=2147483647&v=beta&t=YXl3CljG1XeFAyMRMNzedpZ5gavmGEOdAR7oavf6B3Q"
         ),
     ];
     let pageInfo = new PageInfo(
