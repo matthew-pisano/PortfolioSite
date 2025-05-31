d209c900ef185a3663a04808149ebb338b0da93f
matthew-pisano <matthewpisano14@gmail.com>
2025-05-31 19:22:33 -0400

    Remove Git Link for Mips Terminal

--- src/pages/school/mipsCmd.js
+++ src/pages/school/mipsCmd.js
@@ -3,7 +3,7 @@ import React from "react";
 import Link from "next/link";
 
 import DefaultWrapper from "@/components/DefaultWrapper";
-import { GitLink, PageInfo, Tile } from "@/lib/pageBuilder";
+import { PageInfo, Tile, TRANSLUCENT } from "@/lib/pageBuilder";
 import { genPageTitle } from "@/lib/utils";
 
 export default function MipsCmd() {
@@ -16,10 +16,24 @@ export default function MipsCmd() {
                     <Link href="https://courses.missouristate.edu/kenvollmar/mars/download.htm" target="_blank">
                         Mars simulator
                     </Link>
-                    . Developed as part of a college assembly course, the command processor leverages the low-level
+                    . Developed as part of my college assembly course, the command processor leverages the low-level
                     properties of assembly to carry out its functions using very lightweight code.
                 </>
             )
+        ),
+        new Tile(
+            (
+                <>
+                    NOTE: As I am currently teaching the class that this assignment was originally submitted to, the
+                    source code has been made temporarily unavailable.
+                </>
+            ),
+            ``,
+            "",
+            [],
+            [],
+            "",
+            { backgroundColor: TRANSLUCENT }
         ),
         new Tile(
             <>Features and Function</>,
@@ -39,8 +53,7 @@ export default function MipsCmd() {
         "MIPS Terminal",
         "A simple command processor written in the MIPS assembly language",
         { backgroundColor: "#b3645d" },
-        ["academic", "assembly"],
-        [new GitLink("https://github.com/matthew-pisano/MIPS-Command-Processor", "MIPS Cmd")]
+        ["academic", "assembly"]
     );
     return <DefaultWrapper pageInfo={pageInfo} tiles={tiles} />;
 }
