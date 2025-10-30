396c9a99b2b84f732719e8138e1b6b70fea17804
matthew-pisano <matthewpisano14@gmail.com>
2025-10-29 23:57:40 -0400

    Add Draft of Fahrenheit 451

--- src/components/readingList/scifiBooks.js
+++ src/components/readingList/scifiBooks.js
@@ -203,6 +203,92 @@ export default [
         "house-of-leaves"
     ),
     new BookTile(
+        <>Fahrenheit 451</>,
+        "Ray Bradbury",
+        (
+            <>
+                Guy Montag is a <i>fireman</i>. When an emergency is called in to his station, he, like his comrades
+                across the country, slides down his fireman's pole, dons his flame retardant suit, and races to the
+                scene in a fire engine. Pulling up to the fire-proof house, as all houses now are, with kerosene in
+                place of water and a need to destroy, rather than protect, he stokes the flames until all threats to the
+                state have been immolated. Every precaution must be taken when it comes to the threat of the printed
+                word.
+                <p>
+                    Guy Montag is a <i>stranger</i>. On his way home from his shift, Montag meets a strange young girl,
+                    Clarisse McClellan. After identifying him as a fireman through his odor of accelerant, she begins
+                    asking him questions...questions that make him think. "Are you happy?" What? Of course he was happy!
+                    He had endless entertainment at his fingertips; in fact, he had just recently gotten a third
+                    ceiling-to-floor television screen installed in his parlor...although he did not find the endless
+                    stream of melodramas very entertaining. He was never bored either! The state made it nearly
+                    impossible to be bored, too much temptation to <i>think</i>...although he wished he had time to
+                    think. "Are you in love?" Such a ridiculous notion! He had a lovely wife Mildred whom he loved very
+                    much...although he wished she would watch her nonstop, personalized feed of "The Family" a little
+                    less, perhaps talk to him at night instead of listening to the seashells in her ears that fed her a
+                    constant stream of entertainment and advertisements. "Did firemen used to put out fires?" How could
+                    Clarisse make such an elementary mistake? The state has extensively documented the history of
+                    fire-starting firemen all the way back to the American revolution...although how had they
+                    fireproofed houses in the 1700s? <i>"Have you ever taken a book instead of burning it?"</i> N-no...
+                </p>
+                <p>
+                    Guy Montag is a <i>liar</i>. He had stolen a book, and he had finally worked up the courage to read
+                    it. During one of his late-night calls, he had snatched a book of poetry curing the chaos of the
+                    call. He prayed that Fire Chief Beatty did not notice. This call was different, though. Instead of
+                    the offender being taken away be the authorities, the woma whose library they had just decimated
+                    threw herself into the fire along with it. What could be in those books that was worth dying over?
+                    Calling in sick the day after, he planned to find out. Chief Beatty comes over to make sure he is
+                    feeling alright...he never calls out of a shift. As Montag lies in bed, with the book hidden under
+                    his pillow, the Chief delivers a monologue on why firemen do what they do. As firemen, it is their
+                    duty to protect people from the harm that books can cause and to preserve the stability of the
+                    state. Books inconvenient, he explains. They are unpopular, they are decisive, they are subversive,
+                    they upset people. The state did not ban books, he reveals, the people banned them themselves.
+                    People now live with endless entertainment, personalized to their own perspectives and views. The
+                    entertainment is endless, the televisions know them and can tailor what they see so they are never
+                    bored. Books do not care who you are, they do not yield to your preferences, their philosophies are
+                    only accessible to those few that understand them. Books are <i>destabilizing</i> and the people
+                    want them gone. He goes on to explain that it is normal for books to call to firemen, to temp them
+                    into upsetting the delicate balance of society. It is alright if a fireman gives in and takes a
+                    book, as long as it is burned within a day. You wouldn't happen to know anything about that, would
+                    you Montag?
+                </p>
+                <p>
+                    Guy Montag is a <i>deviant</i>. As Montag enters the fire house for his next shift, Beatty holds out
+                    his hand, expectantly. Guy places the book in his hand. After Beatty further deconstructs any
+                    thoughts of subversion that Guy may still harbour, the alarm bell screams to life. There are more
+                    books to be burned. Tearing through the quiet 3:00 AM streets, they arrive at the offending house.
+                    Ready to torch yet another collection, Montag realizes where they are. This is his house. This most
+                    recent book was not his first offense. He had been collecting and preserving books for months.
+                    Mildred must have called it in. Chief Beatty orders him to burn his collection, after which he will
+                    be immediately arrested for his crimes. Beatty taunts Montag, quoting literature at him as he scolds
+                    Guy for being so foolish, dares him to <i>do something</i> about it. Backed into a corner, Montag
+                    has only one option if he is to stay a free man. He turns from the house and towards Beatty, ignites
+                    the pilot flame on his hose...and lets a stream of burning kerosene spew towards his most immediate
+                    threat.
+                </p>
+                <p>
+                    Guy Montag is a <i>murderer</i>. Tearing off his suit, Montag takes off into the night. The police
+                    were already on their way, he does not have much time. Cutting through back yards and highways, he
+                    races to evade the police and the mechanical hound tracking his scent.
+                </p>
+                <p>
+                    Guy Montag is a <i>fugitive</i>. Peering in through living room windows, he can see hiw own chase
+                    being televised across the city. The people love a spectacle. As the authorities close in, Montag
+                    reaches the river. In an effort to throw the hound off of his scent, he jumps in and lets the
+                    current take him downstream and to the opposite bank. Hours later, no hound has tracked him down.
+                    Montag begins to feel safe. As he wanders down the tracks, he comes across a camp of drifters.
+                    These, however, are no ordinary transients. They are professors and thinkers from the long-abandoned
+                    institution of higher education. They tell Montag that they collect books, just like him. Although
+                    not physically, that would be far too risky. They keep their books in a place where the state cannot
+                    fine them, in their own minds. Each of them has memorized portions of books they they find
+                    meaningful. Together, they form a nationwide mental library.
+                </p>
+            </>
+        ),
+        ``,
+        <></>,
+        "/media/image/fahrenheit-451.jpg",
+        "fahrenheit-451"
+    ),
+    new BookTile(
         <>2001: A Space Odyssey</>,
         "Arthur C. Clarke",
         (
