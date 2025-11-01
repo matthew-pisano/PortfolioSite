d96e9d391e73caa3a1b1ff0fe4a5f15e0f355171
matthew-pisano <matthewpisano14@gmail.com>
2025-11-01 12:19:18 -0400

    Fix Typos in Philosophy Books

--- src/components/readingList/philosophyBooks.js
+++ src/components/readingList/philosophyBooks.js
@@ -12,26 +12,25 @@ export default [
                 a part of France's colonial empire. The first part of the book opens with the death of Meursault's
                 mother and his travel to her old-age home and the subsequent funeral. Meursault's general indifference
                 and lack of grief during the process is our first introduction to his character, including the fact that
-                he is not sure what day she died upon and even what her age was. Throughout the evening vigil and the
+                he is not sure what day she died on or even what her age was. Throughout the evening vigil and the
                 subsequent funeral, the narrator frequently references the extreme heat of the North African
-                environment. This induces stress and clouds his judgement, a fact that will prove to be pivotal later
-                on.
+                environment. This induces stress and clouds his judgment, a fact that will prove pivotal later on.
                 <p>
                     After the funeral, he returns to Algiers where he meets Marie, who previously worked as a secretary
                     at his company. After swimming and viewing a comedy film that day, they begin to form a
                     relationship, at least in Marie's eyes. Meursault enjoys the situation, but is apathetic toward
                     Marie's suggestions of formalizing their relationship. As Meursault is returning home from work one
-                    day, he encounters Salamano, his elderly neighbor. Accompanying the old man is his dog which lost
-                    much of its formerly smooth coat dur to mange. He notes his neighbors twice-daily ritual of taking
-                    his dog out as he curses and beats the animal for not behaving properly.
+                    day, he encounters Salamano, his elderly neighbor. Accompanying the old man is his dog, which lost
+                    much of its formerly smooth coat due to mange. He notes his neighbor's twice-daily ritual of taking
+                    the dog out as he curses and beats the animal for not behaving properly.
                 </p>
                 <p>
                     Soon after, our protagonist becomes involved with Raymond Sintès, his neighbor and rumored pimp.
                     Raymond has convinced himself that his mistress, an Arab woman, has been unfaithful to him. To this
                     end, he employs Meursault to help him get his revenge. Over dinner, he asks the narrator to write a
-                    letter which will trick to woman to return to him so he can execute his reprisal. Despite the fact
-                    that Raymond had earlier proudly described his short temper and pension for violence, Meursault
-                    takes no issue with the moral implications of the situation, rather, he is more interested in the
+                    letter which will trick the woman to return to him so he can execute his reprisal. Despite the fact
+                    that Raymond had earlier proudly described his short temper and penchant for violence, Meursault
+                    takes no issue with the moral implications of the situation; rather, he is more interested in the
                     fact that Raymond now describes him as his 'pal'. When the woman visits Raymond's apartment, he
                     beats her enough for the police to be called. When the officer arrives, he gives Raymond a warning
                     and a court notice. During the session, Meursault serves as a witness of the woman's infidelity
@@ -40,68 +39,68 @@ export default [
                 <p>
                     During this period, Meursault's boss at his firm extends an offer to travel to their new Paris
                     branch. He has no strong feelings about moving, going with whatever would seem to please his
-                    superior; this draws his boss to criticise him on his lack of motivation. When Marie brings up the
+                    superior; this draws his boss to criticize him for his lack of motivation. When Marie brings up the
                     topic of marriage, he reacts similarly. During another trip home from work, we meet Salamano again.
                     We learn that he has recently lost his dog. Despite his obvious ire for his pet, he is extremely
-                    distraught. Meursault learns that he adopted the dog shortly after his wife's passing. Meursault
-                    takes note of his neighbor's contradictory behavior but does not offer any opinion to the reader.
+                    distraught. Meursault learns that Salamano adopted the dog shortly after his wife's passing.
+                    Meursault notes his neighbor's contradictory behavior but does not offer any opinion to the reader.
                 </p>
                 <p>
-                    Soon after, Raymond invites Meursault and Maria to a weekend at his friend's bungalow on the beach.
-                    On the way, the group encounters several Arab men that have been recently been tailing Raymond; one
-                    of these men is the brother of his former mistress. At the beach, Meursault, Raymond, and his friend
-                    Masson encounter the group of Arabs again. This time, Raymond initiates a fight where he is badly
-                    slashed with a knife. During the scuffle, Raymond nearly draws his revolver on his assailant, but is
-                    persuaded by Meursault to give him the weapon instead to defuse the situation. The unnamed Arab men
-                    run off after this event.
+                    Soon after, Raymond invites Meursault and Marie to a weekend at his friend's bungalow on the beach.
+                    On the way, the group encounters several Arab men who have recently been tailing Raymond; one of
+                    these men is the brother of his former mistress. At the beach, Meursault, Raymond, and his friend
+                    Masson encounter the group of Arabs again. This time, Raymond initiates a fight during which he is
+                    badly slashed with a knife. During the scuffle, Raymond nearly draws his revolver on his assailant,
+                    but is persuaded by Meursault to give him the weapon instead to defuse the situation. The unnamed
+                    Arab men run off after this event.
                 </p>
                 <p>
-                    Later in the day, Meursault goes on a walk on the beach alone, noting the extreme heat of the day.
-                    On his way to a natural spring that would relieve his suffering, he encounters the Arab man alone.
-                    As the narrator approaches, the man draws the knife that he used to slash Raymond. Delirious from
-                    heat exhaustion, Meursault accidentally shoots the man with Raymond's revolver. After a pause, he
-                    shoots him four more times.
+                    Later in the day, Meursault goes on a walk on the beach alone, noting the extreme heat. On his way
+                    to a natural spring that would relieve his suffering, he encounters the Arab man alone. As the
+                    narrator approaches, the man draws the knife that he used to slash Raymond. Delirious from heat
+                    exhaustion, Meursault shoots the man with Raymond's revolver. After a pause, he shoots him four more
+                    times.
                 </p>
                 <hr />
                 <p>
                     The second part of the book details the fallout of the unnamed man's murder. Meursault is arrested
                     and placed in prison awaiting trial. During his incarceration, he notes that it was easier to
-                    accommodate to prison life than he imagined, due in part to his general detachment to life in
+                    accommodate to prison life than he imagined, due in part to his general detachment from life in
                     general. Over the next several months, he begins to talk to himself in his cell. At several points,
-                    he is brought to the Magistrate for evaluation. Meursault never denies the murder, but also cannot
+                    he is brought to the magistrate for evaluation. Meursault never denies the murder, but also cannot
                     fully articulate why he shot the man. During their meeting, Meursault's apathy sends the magistrate
                     into an impassioned and religious tirade; the narrator expresses his confusion to the reader on how
                     the subject is relevant to his case.
                 </p>
                 <p>
-                    At the trial, the prosecution builds its case on Meursault's character and circumstance far more
-                    heavily than the facts of the case. They point out how he did not cry a his mother's funeral, how he
-                    want to see a comedy movie that same day, and even how he accepted coffee with milk from the home's
-                    caretaker. Meursault is described very passionately as a soulless monster for his quietness and lack
-                    of empathy. The weight of these allegations thoroughly surprises Meursault who does not view himself
-                    as such. The defense calls several character witnesses on his behalf and his attorney assures
-                    Meursault that the sentence will likely be light if found guilty.
+                    At the trial, the prosecution builds its case on Meursault's character and circumstances far more
+                    heavily than the facts of the case. They point out how he did not cry at his mother's funeral, how
+                    he went to see a comedy movie that same day, and even how he accepted coffee with milk from the
+                    home's caretaker. Meursault is passionately described as a soulless monster for his quietness and
+                    lack of empathy. The weight of these allegations thoroughly surprises Meursault, who does not view
+                    himself as such. The defense calls several character witnesses on his behalf, and his attorney
+                    assures Meursault that the sentence will likely be light if he is found guilty.
                 </p>
-                <p>Meursault is sentenced to execution by guillotine 'in the name of the French people'.</p>
+                <p>Meursault is sentenced to execution by guillotine "in the name of the French people."</p>
                 <p>
                     As he awaits his execution, Meursault obsesses over the idea of a successful appeal and possible
-                    ways of escape as he attempts to come to terms with his impending doom. We wonders how his death can
-                    be at the behest of the nebulous notion of 'the French people'. After refusing the prison chaplain
-                    several times, he visits Meursault's cell regardless. The chaplain preaches that the narrator must
-                    come to terms with his actions to be forgiven by God. He insists that this is the only path forward.
-                    This pushes Meursault past his threshold, which has already been worn thin. He shouts at the
-                    chaplain, something he has never been shown to do. He does not believe in God, nor does he care to.
-                    In a rage, Meursault attacks the priest's philosophy and patronizing attitude towards him. He
-                    asserts that there is no point in seeking God's forgiveness or hoping for his appeal since he is
-                    condemned regardless, be it in a week by execution or in twenty years by old age. Given this, none
-                    of his actions or desires even matter since the outcome will always be the same.
+                    ways of escape as he attempts to come to terms with his impending doom. He wonders how his death can
+                    be at the behest of the nebulous notion of "the French people." After Meursault refuses the prison
+                    chaplain several times, the chaplain visits his cell regardless. The chaplain preaches that the
+                    narrator must come to terms with his actions to be forgiven by God and insists that this is the only
+                    path forward. This pushes Meursault past his threshold, which has already been worn thin. He shouts
+                    at the chaplain, something he has never done before. He does not believe in God, nor does he seek
+                    to. In a rage, Meursault attacks the priest's philosophy and patronizing attitude toward him. He
+                    asserts that there is no point in seeking God's forgiveness or hoping for an appeal since he is
+                    condemned regardless—whether in a week by execution or in twenty years by old age. Given this, none
+                    of his actions or desires matter since the outcome will always be the same.
                 </p>
                 <p>
                     After prison guards separate the two, Meursault feels drained. He no longer has the energy to hope
-                    for an appeal or a successful escape. He resigns himself to the 'benign indifference of the
-                    universe' and concludes that he is, and always has been happy as he is absorbed into the uncaring
+                    for an appeal or a successful escape. He resigns himself to the "benign indifference of the
+                    universe" and concludes that he is, and always has been, happy as he is absorbed into the uncaring
                     universe. He hopes that, at the date of his public execution, he will be surrounded not by loved
-                    ones, but by a large and hateful crowd.
+                    ones but by a large, hateful crowd.
                 </p>
             </>
         ),
@@ -109,30 +108,30 @@ export default [
         (
             <>
                 The narrator, Meursault, serves as a vessel through which Camus explores a subset of 1940s French
-                society, and human the human condition more generally. A common theme that Camus includes in his story
-                is the fact that the rules and customs of society are not based on any fundamental laws, but rather on
-                vague notions of morality and rule of law. Serving as a detached observer, Meursault is a stranger in
-                this society that he is placed in, a feeling that Camus also imbues the reader with.
+                society and the human condition more generally. A common theme that Camus includes in his story is that
+                the rules and customs of society are not based on any fundamental laws, but rather on vague notions of
+                morality and social convention. Serving as a detached observer, Meursault is a stranger in this society,
+                a feeling that Camus also imparts to the reader.
                 <p>
                     Several blatant absurdities and contradictions are present within the story. The police let Raymond
-                    off with a warning after they determine that the woman that he batters was unfaithful. Salamano
-                    derides and abuses his dog, then experiences a great loss when it escapes. The magistrate is
-                    convinced that Meursault can escape harsh punishment if he repents, then quickly unravels when
-                    Meursault does not reciprocate his conviction. The prosecutor builds his case primarily upon
-                    Meursault's minor behaviors that do not perfectly match the expectations of society. The chaplain
-                    insists that Meursault, a man condemned to a violent death, will be safe and at peace if he only
-                    accepts that the lord gives his life meaning.
+                    off with a warning after they determine that the woman he batters was unfaithful. Salamano derides
+                    and abuses his dog, then experiences a great loss when it disappears. The magistrate is convinced
+                    that Meursault can avoid harsh punishment if he repents, then quickly unravels when Meursault does
+                    not reciprocate his conviction. The prosecutor builds his case primarily on Meursault's minor
+                    behaviors that do not match societal expectations. The chaplain insists that Meursault, a man
+                    condemned to a violent death, will be safe and at peace if he only accepts that the Lord gives his
+                    life meaning.
                 </p>
                 <p>
-                    These incongruities compound upon themselves until Meursault breaks. His already weak connection to
-                    society is severed. He rejects the meanings and interpretations of life that are forced upon him,
-                    instead asserting that no inherent meaning exists. From the point of view of Camus, with Meursault
-                    as his voice, the universe is a fundamentally uncaring place. There is no higher power or force
-                    setting fundamental rules, be it society in general or a god. After Meursault accepts this, he feels
-                    at peace. To him, the ultimate rejection of the norms of society would be to hope that his final
-                    moments are spent surrounded by people who despise him.
+                    These incongruities compound until Meursault breaks. His already weak connection to society is
+                    severed. He rejects the meanings and interpretations of life that are forced upon him, instead
+                    asserting that no inherent meaning exists. From Camus's point of view, with Meursault as his voice,
+                    the universe is fundamentally uncaring. There is no higher power or force setting fundamental rules,
+                    be it society in general or a god. After Meursault accepts this, he feels at peace. To him, the
+                    ultimate rejection of societal norms is to hope that his final moments are spent surrounded by
+                    people who despise him.
                 </p>
-                <p>To many readers, this is can be a rather attractive notion.</p>
+                <p>To many readers, this can be a rather attractive notion.</p>
             </>
         ),
         "/media/image/the-stranger.jpg",
