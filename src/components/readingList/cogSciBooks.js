bdb2d8cdfae7480f4c115a556b32df4747786a33
matthew-pisano <matthewpisano14@gmail.com>
2025-11-28 17:41:02 -0500

    Expand Human Compatible Summary

--- src/components/readingList/cogSciBooks.js
+++ src/components/readingList/cogSciBooks.js
@@ -539,7 +539,7 @@ export default [
                 artificial intelligence research: alignment and the problem of control. Within its pages, Russell argues
                 that advanced artificially intelligent systems will pose a significant, existential risk to humanity.
                 This risk also has a non-trivial chance of being realized in the near future. Despite uncertainty in our
-                ability to actually develop superintelligence {<FootRef idx={1} anchor={`human-compatible`} />},
+                ability to actually develop superintelligence{<FootRef idx={1} anchor={`human-compatible`} />},
                 developing an intelligence using our current techniques could be catastrophic. More specifically, he
                 asserts that the single-goal optimization objectives used to train modern AI systems could lead to
                 dangerous and unforeseen consequences. AI trained in this manner would likely not reflect the values
@@ -547,6 +547,127 @@ export default [
                 To structure his argument, Russell splits the book into three parts focusing on an overview of machine
                 intelligence, realized and foreseen problems with intelligent machines, and potential solutions for the
                 problem of control, respectively.
+                <p>
+                    While our biggest threats lie within superintelligence that we cannot control, Russell still finds
+                    it valuable to consider potential misuses of AI while it is still unintelligent enough for us to
+                    control it. With the attention to detail and parallelism found in most definitions of advanced AI,
+                    these systems would make a secret police fource of unrivaled effectiveness. Systems that never tire
+                    or miss a subtle detail could be used for proliferous surveillance of humans. Even rudimentary
+                    systems of monitoring can be used as effective information sources for blackmail campaigns. As the
+                    book points out, these have been in effect since 2016. Russell also touches on <i>deepfakes</i>,
+                    convincing pictures or videos of someone in a situation that the never really were in. This was
+                    becoming an issue back in 2020; the technology has improved dramatically since then. Efforts to
+                    curtail the spread of their surveillance and misinformation is active in platforms such as Google of
+                    Facebook, but effectiveness was limited and remains so today.
+                </p>
+                <p>
+                    The text also discusses autonomous weapons systems (AWS)
+                    {<FootRef idx={2} anchor={`human-compatible`} />}. Aside from surface level concerns of a future
+                    intelligent agent using these weapons against humans intentionally, a more mundane concern lies at
+                    our current level of technology. AI systems do not always behave as you train them, their actions
+                    can appear to optimize for the desired objective during training, while they are really optimizing
+                    for something else
+                    {<FootRef idx={3} anchor={`human-compatible`} />}. This can quickly become obvious in practice if
+                    not caught.
+                </p>
+                <p>
+                    Advanced autonomous systems can not only replace human soldiers, but also wider sections of the job
+                    market. For the entire history of automation, more mechanized jobs have been reliably followed by
+                    periods of new job creation. Farm laborers were moved to factories when harvesting machines became
+                    more efficient and then to office jobs as simple robots proliferated through factory floors. Like
+                    with factory jobs, limited automation enables an industry to flourish, but the need for workers
+                    falls once again as automation increases. Although, that happens when machines can reliably,{" "}
+                    <i>and generally</i>, automate mental labor in addition to physical labor? Russell gives the example
+                    of house painters to make this picture more clear. In a world where the only paintbrushes were 0.1
+                    millimeters wide, no house painters would be employed. It would simply take too long for any
+                    appreciable work to get done. As brushes grow to a millimeter in thickness, small guilds of artisan
+                    painters form to painstakingly create delicate murals over many dozens of hours. At ten millimeters,
+                    painting no longer requires specialized skills and extensive training, people can now paint homes
+                    with reasonable degrees of efficiency; many new housepainters find jobs at this stage as the field
+                    begins to mature. As brushes give way to rollers and spray guns, much fewer house painters are
+                    needed as each one can perform the same labor as many and the number of houses needing painting does
+                    not expand proportionally. When semi-autonomous machines can paint houses, the number of workers
+                    drops further as one person can oversee the painting of an entire house very quickly. Russell
+                    further explores this trend from the lens of white collar workers as well, with similar predicted
+                    (and observed!) results. Finally, he discusses the potential consequences of making automata
+                    indistinguishable from humans in a physical sense, in addition to in an intellectual sense. In short
+                    the integration of humanoid robots into distinctly human jobs (healthcare and law, for example),
+                    would have severe negative affects on human socialization and dignity.
+                </p>
+                <p>
+                    There are several problems that arise when AI becomes more generally intelligent and capable than
+                    humans. <i>The Gorilla Problem</i> illustrates the future dynamic between humans and AI systems. Ten
+                    million years ago, ancient hominins branches off from gorillas and other grate apes. Early on, our
+                    ancestors were few and the apes many. A <i>directed</i> effort could have easily eliminated our
+                    lineage. As we evolved, we became more generally intelligent and capable than our relatives.
+                    Millions of years later, it is our species trying to save gorillas from <i>accidental</i> extinction
+                    at our own hand than the other way around. If we create advanced AI that is difficult to control, we
+                    will be in the position of the gorillas with no reason to think that the machines will pay much mind
+                    to our own <i>accidental</i> extinction{<FootRef idx={4} anchor={`human-compatible`} />}.{" "}
+                    <i>The King Midas Problem</i> threatens that advanced AI is likely to give us exactly what we ask
+                    for, <i>us exactly what we ask for</i>. Just as King Midas' wish turned his food and drink to
+                    inedible gold, so could our own wishes lead to unforeseen consequences. As Nick Bostrom discusses at
+                    length in <i>Superintelligence</i>, machines designed without sufficient planning are likely to
+                    suffer from <i>value alignment</i>. Their values/goals/objectives may be exactly what we ask for,
+                    but not exactly what we mean. In addition to the many, many examples of how this can pose an
+                    existential risk, a more grounded example is often helpful. Click-through and engagement algorithms
+                    are currently a staple of most internet advertising services. This is chiefly used in search engines
+                    and social media. These algorithms optimize for a single goal: keep users engaging with the platform
+                    as long as possible so they may be shown more and better targeted advertisements. This works well in
+                    theory (better content should attract more viewers), but in reality, this often drives
+                    misinformation or hateful content {<FootRef idx={5} anchor={`human-compatible`} />}.{" "}
+                    <i>Unaligned Instrumental Goals</i> pose another problem. Even if the terminal, or final, goals of a
+                    machine are somehow aligned with our intentions, that does not guarantee that instrumental or
+                    intermediate goals will be. You can't achieve your terminal goals if you wre dead, so
+                    self-preservation becomes a strong instrumental motivator, even of not explicitly trained into a
+                    model{<FootRef idx={6} anchor={`human-compatible`} />}. Compounding these issues is that of an{" "}
+                    <i>Intelligence Explosion</i>. It would potentially be easier to solve the above problems if we
+                    could ensure that the intelligence of artificial minds always grew smoothly and linearly with time.
+                    We could carefully craft machines up until just before they become too clever for us to reliably
+                    control without having to face the full reality of an existential risk. This is not likely to be the
+                    case. With or without human encouragement, the increase of one's intelligence is an instrumental
+                    goal applicable to nearly every unbounded or underspecified task. Systems <i>less</i> intelligent
+                    than we are could, overtly or covertly, begin to slightly increase their own intelligence. The
+                    second, smarter system would be motivated to do the same. This process occurs recursively until
+                    further increases in capability are subject to sufficiently diminishing returns. The values of a
+                    system before this exponential curve are not guaranteed to remain the same after logistic terms
+                    begin to dominate and the process slows.
+                </p>
+                <p>
+                    Like many threats that largely live in the realm of theory (at least in the existential sense),
+                    there is still significant debate among researchers in the field on how to solve the problem of
+                    control. Unfortunately, these is still discourse on whether this is even a problem at all. Russell
+                    points out examples of AI researchers denying that the problem of control exists or is as serious as
+                    people make it out to be. Superintelligent AI could be framed as impossible, there is no need to
+                    worry about something that cannot happen. It could be in the far off future, why dedicate resources
+                    to something that <i>may</i> not happen for decades? A strong appeal to authority can also be used
+                    to extinguish constructive debate; why should we worry if a handful of accomplished scientists are
+                    not personally worried{<FootRef idx={7} anchor={`human-compatible`} />}? Researchers in the field of
+                    AI may also deflect sound proposals for alignment strategies or preparation, while acknowledging
+                    their technical validity. Likely the most straightforward warnings given by AI safety researchers is
+                    to simply slow down research. Many scientists may agree with this in principle, but their employment
+                    likely hinges on doing the precise opposite of this. Another common argument Russel observes
+                    disengages with the topic of AI safety by introducing a new topic that should be attended to
+                    instead. Oftentimes, proposals advocating for decelerating AI research are met with criticism for
+                    not considering the potential benefits of AI. The key insight here is as follows:{" "}
+                    <i style={{ display: "block", margin: "10px 0px", textAlign: "center" }}>
+                        The potential benefits of advanced AI are incalculable, however, if we fail to carefully align
+                        them, we will not see those benefits; we shall instead incur incalculable harm.
+                    </i>
+                    The text also addresses some common arguments against the thread of superintelligent AI. Shutting
+                    the systems down, boxing them, collaborating with them, or avoiding training them on strict human
+                    goals are all potentially "easy" solutions to the problem of control. Simple solutions like these
+                    fundamentally misunderstand the dynamic between ourselves an a superintelligent agent: it can, and
+                    will out-think us in every scenario. It could prevent itself from being shut down through
+                    intimidation or force, it could convince (just one of) us to let it out of its box, it would (after
+                    a certain point) have no need to collaborate with us, and deciding the "right" goals, if any, is a
+                    highly non-trivial problem. Each of these points, and an AI's motivation for insubordination, are
+                    discussed more thoroughly in my analysis of{" "}
+                    <Link href={"https://matthewpisano.com/readingList#superintelligence"}>
+                        <i>Superintelligence</i>
+                    </Link>
+                    .
+                </p>
             </>
         ),
         (
@@ -572,6 +693,46 @@ export default [
             <>
                 <FootNote idx={1} anchor={`human-compatible`}>
                     At the time of writing and to a somewhat lesser extent today.
+                </FootNote>
+                <FootNote idx={2} anchor={`human-compatible`}>
+                    Unrelated to Amazon Web Services (AWS, although services for the former could certainly be provided
+                    through the latter.
+                </FootNote>
+                <FootNote idx={3} anchor={`human-compatible`}>
+                    <i>Artificial Intelligence as a Positive and Negative Factor in Global Risk</i> (Yudkowsky, 2008),
+                    section 7.2 "An Example of Technical Failure".
+                </FootNote>
+                <FootNote idx={4} anchor={`human-compatible`}>
+                    We like to think of ourselves as operating outside of nature, but habitat destruction can effect us
+                    too. AI systems do not need malice encoded into their goals to tile the planet's surface in solar
+                    panels or use boiling oceans as a form of ablative thermal regulation. Either way, Earth would
+                    quickly be uncomfortable to live in.
+                </FootNote>
+                <FootNote idx={5} anchor={`human-compatible`}>
+                    I personally find this to be one of the more interesting quirks of human behavior. Information that
+                    upsets or scares us spreads much more quickly and with more passion and information that is
+                    positive. This is most apparent in news feeds or social media posts and has been documented at
+                    length in literature. Why are we (myself included, unfortunately) so driven by information that
+                    makes us hate ourselves and each other?
+                </FootNote>
+                <FootNote idx={6} anchor={`human-compatible`}>
+                    <i>Superintelligence</i> expands upon this point in much greater detail. This is a common trope in
+                    fiction as well. <i>2001</i>'s HAL, Asimov's third law of robotics, and Philip K. Dick's Androids
+                    all serve as good examples of when this instrumental goal conflicts with human intentions. It is
+                    difficult to say whether the fact that this trope has strong theoretical backing is satisfying or
+                    terrifying.
+                </FootNote>
+                <FootNote idx={7} anchor={`human-compatible`}>
+                    In my opinion, this is and laziest and potentially most dangerous form of argument. This strategy
+                    refuses to even engage with the concept of a debate. By deferring reasoning off to a perceived
+                    authority, any and all arguments can be absorbed by that person's eminence in their field.{" "}
+                    <i>Ethos</i> is one of Aristotle's core three modes of persuasion for a good reason; people are
+                    often eager to take mental shortcuts and offload complex reasoning to someone that has already
+                    completed said reasoning (and has come to an agreeable conclusion). When considering AI alignment,
+                    it is important to remember that this is an <i>unsolved problem</i>. Nobody knows exactly how to
+                    solve, or even approach, this issue even experts in the field of artificial intelligence. Given that
+                    no solution is yet known, it is imperative to consider the reasoning of a strategy on its own
+                    merits, falling back to expert opinion only as a last resort.
                 </FootNote>
             </>
         ),
