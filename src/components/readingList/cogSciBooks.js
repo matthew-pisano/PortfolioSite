e4320d80555cfee766025a0a5a8fa7498be39e91
matthew-pisano <matthewpisano14@gmail.com>
2025-11-29 11:14:57 -0500

    Add Final Sections to Human Compatible Summary

--- src/components/readingList/cogSciBooks.js
+++ src/components/readingList/cogSciBooks.js
@@ -718,6 +718,42 @@ export default [
                     sucralose that provide no energy. Note that the goal of eating artificially sweet foods has nothing
                     to do with the "intended" goal, but it satisfies the goal of reward all the same.
                 </p>
+                <p>
+                    Even if we could design an AI system that could reliably learn a human's goals and act out their
+                    intentions, there are still complications. Humans are many and often have conflicting, even
+                    malicious, goals; if a machine is supposed to learn from humanity, which of those goals should it
+                    learn? A common misconception is that learned robots will need to form one coherent value system,
+                    which can cause issues for different cultures of belief systems. Russell argues that this is not the
+                    case, however. A successful robot would only need to learn to <i>predict</i> what individuals prefer
+                    in isolation. Another complication is that there many humans, with directly conflicting preferences.
+                    There are several solutions to this problem. Robots could simply ignore the preferences of everyone
+                    except for their owner. This is straightforward, but many support the idea of automation as a
+                    collective good, rather than toys exclusively for those that control them.
+                </p>
+                <p>
+                    A more workable approach is to make utilitarian robots. This sounds ideal, but it quickly evolves
+                    into philosophical conjecture without a unified strategy. For example <i>utility monsters</i> are
+                    people who theoretically experience pleasure significantly more than others, and thus a
+                    utility-maximizing robot would allocate disproportional resources to these people
+                    {<FootRef idx={9} anchor={`human-compatible`} />}. Additionally, how much weight would a
+                    utility-maximizing robot assign to its owner over others. At either end of this scale, you either
+                    get selfish robots all over again or robots so altruistic that there is no personal advantage to
+                    buying one at all. Finding a balance that makes this idea practically workable will be difficult.
+                </p>
+                <p>
+                    Another complication is that humans do not behave in isolation. In addition to our own happiness, we
+                    also derive happiness from each others benefit or detriment. Humans can have altruistic tendencies,
+                    where they enjoy the happiness of others for its own sake, independent of direct improvements to
+                    their condition. There are also <i>negative</i> altruists that derive happiness from the lowered
+                    standards of living of others. Negative altruists need not be pure evil either; anyone who has ever
+                    felt envy or hoped for misfortune to befall someone else has experienced negative altruism
+                    {<FootRef idx={10} anchor={`human-compatible`} />}. Humans are also prone to emotional outbursts,
+                    irrationality, and are often myopic. It would important for a robot to recognize that these altered
+                    states do not represent a change in our goals, but are rather consequences of our limitations. We
+                    may not see a clear avenue to our goals and may act erratically from the perspective of a rational
+                    agent. It is important for robots to be able to model and predict these states without directly
+                    experiencing them internally.
+                </p>
             </>
         ),
         (
@@ -793,6 +829,20 @@ export default [
                 </FootNote>
                 <FootNote idx={8} anchor={`human-compatible`}>
                     My opinion.
+                </FootNote>
+                <FootNote idx={9} anchor={`human-compatible`}>
+                    An idea related to this single-minded optimization (wait, haven't we already argued against this?)
+                    is the <i>Repugnant Conclusion</i>. If a group of people have a good quality of life based on some
+                    limited supply of resources, it would reason that a much larger group with a slightly lower quality
+                    of life would be better if resources can be allocated more efficiently. This reasoning can be
+                    applied iteratively until there are innumerable people living barely above subsistence level.
+                </FootNote>
+                <FootNote idx={10} anchor={`human-compatible`}>
+                    Like Sapolsky discusses in <i>Behave</i>, this is not exclusively a bad thing, we evolved these
+                    feelings for a reason: to ensure fairness within a group. When an individual is extremely wealthy in
+                    a resource constrained environment, it is reasonable to hope that they decrease their own living
+                    standard in exchange for raising that of everyone else. Of course, it is important to remember that
+                    many instances of envy do indeed produce net-negative effects.
                 </FootNote>
             </>
         ),
