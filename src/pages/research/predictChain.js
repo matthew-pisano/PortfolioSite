fa6f4963871c845a41b7f27d15dbfaee39fa6123
matthew-pisano <matthewpisano14@gmail.com>
2025-12-19 22:22:53 -0500

    Update PredictChain

--- src/pages/research/predictChain.js
+++ src/pages/research/predictChain.js
@@ -3,27 +3,35 @@ import React from "react";
 import Link from "next/link";
 
 import DefaultWrapper from "@/components/DefaultWrapper";
-import { GitLink, PageInfo, SectionTile, Tile, TileLink } from "@/lib/pageBuilder";
+import { Tile, SectionTile } from "@/components/Tiles";
+import { GitLink, PageInfo, TileInfo, TileLink } from "@/lib/pageBuilder";
 import { PageColor } from "@/lib/themes";
 import { genPageTitle } from "@/lib/utils";
 
 export default function PredictChain() {
-    let tiles = [
-        new Tile(
-            <h2>Overview</h2>,
-            (
+    let pageInfo = new PageInfo(
+        genPageTitle(__filename),
+        "PredictChain",
+        "Empowering collaboration and data accessibility for AI in a decentralized blockchain-based marketplace",
+        { backgroundColor: PageColor.URANIUM_GREEN },
+        ["research", "academic", "ai", "python", "hack"],
+        [
+            new GitLink("https://github.com/predict-chain/predict-chain", "PredictChain"),
+            new TileLink("https://arxiv.org/abs/2307.15168", "Research Paper")
+        ]
+    );
+    return (
+        <DefaultWrapper pageInfo={pageInfo}>
+            <Tile tileInfo={new TileInfo({ title: <h2>Overview</h2>, thumbnail: "/media/image/predictChain.png" })}>
                 <>
                     <i>PredictChain</i> is a decentralized artificial intelligence marketplace that I developed,
                     primarily along with another student at RPI. It utilizes the <i>Algorand</i> blockchain to allow
                     users to host instances of this marketplace, with the goal of more freely distributing AI models,
                     data, and compute among all users.
                 </>
-            ),
-            "/media/image/predictChain.png"
-        ),
-        new Tile(
-            <>Abstract (Abridged)</>,
-            (
+            </Tile>
+
+            <Tile tileInfo={new TileInfo({ title: <>Abstract (Abridged)</> })}>
                 <>
                     Limited access to computing resources and training data poses significant challenges for individuals
                     and groups aiming to train and utilize predictive machine learning models. Although numerous
@@ -36,20 +44,16 @@ export default function PredictChain() {
                     characteristics. Our decentralized approach empowers users to develop improved models accessible to
                     the public, promotes data sharing, and reduces reliance on centralized cloud providers.
                 </>
-            )
-        ),
-        new Tile(
-            <>Conferences and Publications</>,
-            (
+            </Tile>
+
+            <Tile tileInfo={new TileInfo({ title: <>Conferences and Publications</> })}>
                 <>
                     This paper was accepted by, and presented at, the ChainScience 2023 conference. Our paper is freely
                     available on arXiv for public viewing and citation.
                 </>
-            )
-        ),
-        new Tile(
-            <>Competitions and Awards</>,
-            (
+            </Tile>
+
+            <Tile tileInfo={new TileInfo({ title: <>Competitions and Awards</> })}>
                 <>
                     We worked on this project as part of the{" "}
                     <Link href="https://mega-ace.org/hackathon/" target="_blank" rel="noreferrer">
@@ -58,12 +62,11 @@ export default function PredictChain() {
                     . At the local competition, PredictChain won the 'Most Innovative Use of Technology' award and 3rd
                     place overall. At the global competition, our project won 1st place overall!
                 </>
-            )
-        ),
-        new SectionTile(<>Project Details</>),
-        new Tile(
-            <>Background</>,
-            (
+            </Tile>
+
+            <SectionTile tileInfo={new TileInfo({ title: <>Project Details</> })} />
+
+            <Tile tileInfo={new TileInfo({ title: <>Background</> })}>
                 <>
                     This project, and the paper associated with it was developed as part of RPI's AI and Blockchain
                     course. Over the course of the semester, we worked on this project. It was a collaborative effort as
@@ -71,11 +74,9 @@ export default function PredictChain() {
                     hackathon. My responsibility was developing the models, blockchain communication protocol, and
                     backend, while my partner was tasked with the front-end and user management systems.
                 </>
-            )
-        ),
-        new Tile(
-            <>Architecture</>,
-            (
+            </Tile>
+
+            <Tile tileInfo={new TileInfo({ title: <>Architecture</>, thumbnail: "/media/image/predictChainFull.png" })}>
                 <>
                     PredictChain is comprised of two main parts. The Oracle node is the hub of each PredictChain
                     instance. It is run by a host of the network. Users can connect to any active Oracle to interact
@@ -85,12 +86,9 @@ export default function PredictChain() {
                     better rate is offered. In addition to hosting the network, Oracles also have the responsibility of
                     managing uploaded datasets and both training and performing inference on models.
                 </>
-            ),
-            "/media/image/predictChainFull.png"
-        ),
-        new Tile(
-            <>User Actions</>,
-            (
+            </Tile>
+
+            <Tile tileInfo={new TileInfo({ title: <>User Actions</> })}>
                 <>
                     Users of PredictChain can perform three major actions. Users can upload a dataset of their choosing,
                     request that a model be trained on any user-uploaded dataset, or a request for a model query. All
@@ -100,11 +98,9 @@ export default function PredictChain() {
                     compensate users for the usage of their models or datasets. They are rewarded every time their
                     trained model or uploaded dataset is used.
                 </>
-            )
-        ),
-        new Tile(
-            <>Models</>,
-            (
+            </Tile>
+
+            <Tile tileInfo={new TileInfo({ title: <>Models</> })}>
                 <>
                     PredictChain offers a variety of models for users to interact with. Each of these model types is
                     randomly initialized originally, with users training a copy of that model when requested.
@@ -116,19 +112,7 @@ export default function PredictChain() {
                         <li>Gated Recurrent Unit Networks</li>
                     </ul>
                 </>
-            )
-        )
-    ];
-    let pageInfo = new PageInfo(
-        genPageTitle(__filename),
-        "PredictChain",
-        "Empowering collaboration and data accessibility for AI in a decentralized blockchain-based marketplace",
-        { backgroundColor: PageColor.URANIUM_GREEN },
-        ["research", "academic", "ai", "python", "hack"],
-        [
-            new GitLink("https://github.com/predict-chain/predict-chain", "PredictChain"),
-            new TileLink("https://arxiv.org/abs/2307.15168", "Research Paper")
-        ]
+            </Tile>
+        </DefaultWrapper>
     );
-    return <DefaultWrapper pageInfo={pageInfo} tiles={tiles} />;
 }
