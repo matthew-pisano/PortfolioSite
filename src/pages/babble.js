a31df63469e8f1b1eaed2e9585aca21f605d5629
matthew-pisano <matthewpisano14@gmail.com>
2025-12-20 00:43:24 -0500

    Update Babble

--- src/pages/babble.js
+++ src/pages/babble.js
@@ -1,7 +1,9 @@
 import React, { useEffect } from "react";
 
+import { Tile } from "@/components/Tiles";
 import Wrapper from "@/components/Wrapper";
-import { buildPage, PageInfo, Tile } from "@/lib/pageBuilder";
+import { buildTags, PageInfo, TileInfo } from "@/lib/pageBuilder";
+import tileStyles from "@/styles/pageTiles.module.css";
 import styles from "@/styles/Wrapper.module.css";
 
 /**
@@ -47,15 +49,13 @@ async function babelTilesStep() {
     );
 
     let radii = ["10px", "20px", "50px"];
-    let tileIndex = 0;
-    // For each tile on the page
-    while (document.getElementById("babbleTile" + tileIndex + "Title")) {
-        document.getElementById("babbleTile" + tileIndex).style.borderRadius =
-            radii[Math.floor(Math.random() * radii.length)];
-        let titleElem = document.getElementById("babbleTile" + tileIndex + "Title");
+    let pageTiles = document.getElementsByClassName(tileStyles.displayTile);
+    for (let tile of pageTiles) {
+        tile.style.borderRadius = radii[Math.floor(Math.random() * radii.length)];
+        let titleElem = tile.children[0].children[0];
 
         if (!titleElem.classList.contains("forceWrap")) titleElem.classList.add("forceWrap");
-        let contentElem = document.getElementById("babbleTile" + tileIndex).children[0].children[2];
+        let contentElem = tile.children[0].children[1];
         if (!contentElem.classList.contains("forceWrap")) contentElem.classList.add("forceWrap");
 
         // Add random text to the tile
@@ -65,7 +65,6 @@ async function babelTilesStep() {
         contentElem.style.fontSize = Math.floor(Math.random() * 10) + 10 + "px";
         for (let i = 0; i < Math.floor(Math.random() * 5); i++)
             contentElem.innerHTML += "<br><br>&nbsp;" + randText(Math.floor(Math.random() * 400) + 100);
-        tileIndex++;
     }
 }
 
@@ -82,13 +81,6 @@ async function babbleLoop() {
 }
 
 export default function Babble() {
-    let tiles = [
-        new Tile(<>_</>, <></>),
-        new Tile(<>_</>, <></>),
-        new Tile(<>_</>, <></>),
-        new Tile(<>_</>, <></>),
-        new Tile(<>_</>, <></>)
-    ];
     let pageInfo = new PageInfo("babble", "", "", { backgroundColor: "#5a3afa00" }, ["help"]);
 
     // Start the babble loop when the page loads
@@ -104,7 +96,17 @@ export default function Babble() {
                 </h1>
                 <br />
             </div>
-            {buildPage(pageInfo, tiles)}
+            <div
+                id="tileHolder"
+                className={`w3-display-container ${tileStyles.tileHolder}`}
+                style={pageInfo.holderStyle}>
+                {buildTags(pageInfo.tags, pageInfo.links, true)}
+                <Tile tileInfo={new TileInfo({ title: <>_</> })}></Tile>
+                <Tile tileInfo={new TileInfo({ title: <>_</> })}></Tile>
+                <Tile tileInfo={new TileInfo({ title: <>_</> })}></Tile>
+                <Tile tileInfo={new TileInfo({ title: <>_</> })}></Tile>
+                <Tile tileInfo={new TileInfo({ title: <>_</> })}></Tile>
+            </div>
         </Wrapper>
     );
 }
