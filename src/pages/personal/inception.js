a81a9ddc7179ed10b2cb06e84ce3eafea56c048b
matthew-pisano <matthewpisano14@gmail.com>
2025-12-19 21:45:15 -0500

    Update Inception

--- src/pages/personal/inception.js
+++ src/pages/personal/inception.js
@@ -1,35 +1,12 @@
 import React from "react";
 
 import DefaultWrapper from "@/components/DefaultWrapper";
-import { GitLink, PageInfo, Tile } from "@/lib/pageBuilder";
+import { Tile } from "@/components/Tiles";
+import { GitLink, PageInfo, TileInfo } from "@/lib/pageBuilder";
 import { PageColor } from "@/lib/themes";
 import { genPageTitle } from "@/lib/utils";
 
 export default function Inception() {
-    let tiles = [
-        new Tile(
-            <h2>Overview</h2>,
-            (
-                <>
-                    Inception Collage uses Java's built in graphics libraries, AWT and Swing, to allow users to create
-                    picture mosaics using a GUI.
-                </>
-            )
-        ),
-        new Tile(
-            <>Features and Function</>,
-            (
-                <>
-                    The GUI allows the user to select one image file as the basis of the mosaic, this is the image that
-                    the smaller images will compose. The user then selects any number of other images. These will be
-                    overlaid on top of the background image based on their color and composition. With enough of these
-                    images, the illusion of the original image appears. When zooming in on the new, large image,
-                    however, all of its constituent images are revealed.
-                </>
-            ),
-            "/media/image/inceptionDog.bmp"
-        )
-    ];
     let pageInfo = new PageInfo(
         genPageTitle(__filename),
         "Inception Collage",
@@ -38,5 +15,27 @@ export default function Inception() {
         ["personal", "java"],
         [new GitLink("https://github.com/matthew-pisano/InceptionCollage", "Inception")]
     );
-    return <DefaultWrapper pageInfo={pageInfo} tiles={tiles} />;
+    return (
+        <DefaultWrapper pageInfo={pageInfo}>
+            <Tile tileInfo={new TileInfo({ title: <h2>Overview</h2> })}>
+                <>
+                    Inception Collage uses Java's built in graphics libraries, AWT and Swing, to allow users to create
+                    picture mosaics using a GUI.
+                </>
+            </Tile>
+
+            <Tile
+                tileInfo={
+                    new TileInfo({ title: <>Features and Function</>, thumbnail: "/media/image/inceptionDog.bmp" })
+                }>
+                <>
+                    The GUI allows the user to select one image file as the basis of the mosaic, this is the image that
+                    the smaller images will compose. The user then selects any number of other images. These will be
+                    overlaid on top of the background image based on their color and composition. With enough of these
+                    images, the illusion of the original image appears. When zooming in on the new, large image,
+                    however, all of its constituent images are revealed.
+                </>
+            </Tile>
+        </DefaultWrapper>
+    );
 }
