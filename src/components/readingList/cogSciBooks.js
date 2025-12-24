bdab3062d336f0f5e4ddb372f8785fce8583f990
matthew-pisano <matthewpisano14@gmail.com>
2025-12-24 15:06:50 -0500

    Begin I Am a Strange Loop

--- src/components/readingList/cogSciBooks.js
+++ src/components/readingList/cogSciBooks.js
@@ -907,6 +907,82 @@ export default (
         />
 
         <BookTile
+            title={"I Am a Strange Loop"}
+            author={"Douglas Hofstadter"}
+            synopsis={
+                <>
+                    <i>I Am a Strange Loop</i> serves as a refinement of the ideas Douglas Hofstadter originally
+                    presented in <i>Gödel, Escher, Bach: An Eternal Golden Braid</i>, thirty years earlier. In his prior
+                    work, Hofstadter attempted to explore his ideas on Souls, Selves and <i>I's</i> through the lense of{" "}
+                    <i>Gödel, Escher, Bach</i>'s three namesakes. The "strange" recursive and self-referential natures
+                    of their work served as an anchor for the author to ground his own ideas on the human mind in. By
+                    contrast, <i>I Am a Strange Loop</i> uses these references less as the core of his thesis and more
+                    as supporting elements. Instead, he uses less abstract examples to present his ideas to the reader.{" "}
+                    <i>Symballs</i>, <i>Huneker</i> units, video feedback, and others are used to illustrate the text's
+                    thesis on what an <i>I</i> exactly is, what can be assigned the coveted status of an <i>I</i>, and
+                    how an <i>I</i> can be created from <i>I</i>-less material.
+                    <p>
+                        What exactly is an <i>I</i>? To Hofstadter, it relates closely to the degree of consciousness
+                        associated with a being. Tying this ephemeral quality to something more "real", the text
+                        introduces the notion of a <i>Soul</i>, though only in a secular sense. Small souled beings have
+                        no, or very limited, conscious thought, higher aspirations, or self-referential manners of
+                        thinking. Examples include rocks, microorganisms, or mosquitoes
+                        <FootRef idx={1} />. According to the book, larger souled beings, in roughly increasing order,
+                        include bees, fish, chickens, dogs, infants, adults with severe cognitive impairments, and
+                        finally healthy adult humans. Notably, this list is not based simply on an entity's similarity
+                        to humans
+                        <FootRef idx={2} />. Instead, the size of an entity's soul is correlated only to its cognitive
+                        abilities for theory of mind, self-referential thought, and similar abilities. This measurement
+                        of a soul is also completely divorced from the substrate for cognition. Fictional robots, like{" "}
+                        <i>C-3PO</i> and <i>R2-D2</i> are only made of seemingly inert metal, yet they appear to have
+                        souls comparable to humans. How can this be?
+                    </p>
+                    <p>
+                        Upon closer inspection, humans appear to be made of similarly inert "stuff" to robots or
+                        inanimate objects. We reside, more or less
+                        <FootRef idx={3} />, within our brains. However our brains are composed of neurons, which are
+                        composed of organelles, which are made up by various proteins and molecules, which themselves
+                        are made of atoms, then sub-atomic particles. You would be hard-pressed to find someone claiming
+                        that quarks or strings or hypergraphs are themselves conscious
+                        <FootRef idx={4} />, so how can we, conscious <i>I's</i>, ourselves be conscious? The key,
+                        Hofstadter argues, is not in the material, it lies in patterns of interaction. To better
+                        illustrate this thesis, the text introduces Small Interacting Magnetic Marbles (<i>Simms</i>).
+                        these Simms are constantly in motion, bouncing around within the billiard table-like{" "}
+                        <i>Careenium</i>. Suppose that the walls of this table occasionally flex inward, changing the
+                        trajectory of Simms as they bounce off of it. Suppose further that these magnetic marbles can
+                        fuse together in clumps or fission into components, depending on how other Simms interact with
+                        them. From the perspective of a simm, these <i>Simmballs</i> appear to be massive, immovable
+                        conglomerates of Simms. Zooming out and speeding up, the Simms themselves disappear into a
+                        background blur anf the much larger Simmballs begin to noticeably move. Now, we only see the
+                        Simmballs as they interact with each other, fission apart, fuse, and evolve into complex
+                        patterns. Though they compose the Simballs, the Simms know nothing of these complex movements
+                        that they orchestrate. Only when zooming out do emergent patterns of interaction emerge.
+                        Importantly, there is no <i>Prime Mover</i> here, no special Simms with free will of their own
+                        or dualist homunculi determining how the Simmballs interact. The only determining factor here is
+                        simple physics, yet diverse patterns still emerge.
+                    </p>
+                </>
+            }
+            thoughts={<></>}
+            footnotes={
+                <>
+                    <FootNote idx={1}>
+                        Though Hofstadter describes himself as valuing nearly all living beings, he makes it a point
+                        that mosquitoes, specifically, are very much not on that list.
+                    </FootNote>
+                    <FootNote idx={2}>
+                        I think it is probable that the author would place the most advanced corvids above our most
+                        primitive, yet still closely related, primate cousins.
+                    </FootNote>
+                    <FootNote idx={3}>More to come on this notion later.</FootNote>
+                    <FootNote idx={4}>Except for, perhaps, some exceptionally devout, animist physicist.</FootNote>
+                </>
+            }
+            thumbnail={"/media/image/i-am-a-strange-loop.jpg"}
+            anchor={"i-am-a-strange-loop"}
+        />
+
+        <BookTile
             title={"The Alignment Problem"}
             author={"Brian Christian"}
             synopsis={
