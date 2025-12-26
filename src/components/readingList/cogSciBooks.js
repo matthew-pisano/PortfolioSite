07dd611209a15f5f493331a3ed9d8f4ecdab13c7
matthew-pisano <matthewpisano14@gmail.com>
2025-12-26 18:42:52 -0500

    Add to I Am a Strange Loop

--- src/components/readingList/cogSciBooks.js
+++ src/components/readingList/cogSciBooks.js
@@ -927,10 +927,10 @@ export default (
                         introduces the notion of a <i>Soul</i>, though only in a secular sense. Small souled beings have
                         no, or very limited, conscious thought, higher aspirations, or self-referential manners of
                         thinking. Examples include rocks, microorganisms, or mosquitoes
-                        <FootRef idx={1} />. According to the book, larger souled beings, in roughly increasing order,
-                        include bees, fish, chickens, dogs, infants, adults with severe cognitive impairments, and
-                        finally healthy adult humans. Notably, this list is not based simply on an entity's similarity
-                        to humans
+                        <FootRef idx={1} />. According to the book, larger souled beings (measured in <i>Huneker</i>{" "}
+                        units), in roughly increasing order, include bees, fish, chickens, dogs, infants, adults with
+                        severe cognitive impairments, and finally healthy adult humans. Notably, this list is not based
+                        simply on an entity's similarity to humans
                         <FootRef idx={2} />. Instead, the size of an entity's soul is correlated only to its cognitive
                         abilities for theory of mind, self-referential thought, and similar abilities. This measurement
                         of a soul is also completely divorced from the substrate for cognition. Fictional robots, like{" "}
@@ -960,6 +960,54 @@ export default (
                         Importantly, there is no <i>Prime Mover</i> here, no special Simms with free will of their own
                         or dualist homunculi determining how the Simmballs interact. The only determining factor here is
                         simple physics, yet diverse patterns still emerge.
+                    </p>
+                    <p>
+                        As a prelude for further discussion on "Strange" loops, the book first introduces more mundane
+                        forms of "loopiness". On the journey to self-reference, we must first consider feedback. The
+                        float-ball of a toilet is a very rudimentary form of feedback. As the water level in its tank
+                        increases, the float rises, eventually shutting off the valve that is filling the tank in the
+                        first place. Feedback and self-reapplication is found at the core of exponential functions, the
+                        output of the last multiplication is fed back into subsequent multiplications. In a concrete
+                        sense, the familiar screech of audio feedback is the result of an amplified sound being fed back
+                        into the amplifier through the microphone. Video feedback is somewhat different, however. In the
+                        case of a simple camera pointing at a screen, very little amplification is present. Instead of
+                        an explosion of intensity, the recursion of images converges on a single, seemingly infinitely
+                        far vanishing point. Here, the original image is faithfully recreated within successor images in
+                        a basic form of self-reference. This self-reference was a major concern among early twentieth
+                        century mathematicians. Near the turn of the century, Bertrand Russell had been working on
+                        grounding all of mathematics inside a basic theory of sets. Self-reference plagued this early
+                        attempt, leading to contradictions at the core of his framework. <i>Russell's paradox</i>, as it
+                        became known, concerns the set of all sets, namely, does it contain itself? A system that
+                        allowed this paradox quickly became unworkable, therefore Russell and his colleague Alfred
+                        Whitehead sought to purge it from their theories. Their new approach, published in{" "}
+                        <i>Principia Mathematica</i> was designed to be immune to such self-references and paradoxes.
+                        Here, mathematical objects were structured in a rigid hierarchy. Objects within this hierarchy
+                        could only be defined, or reference, objects in lower strata. Their <i>magnum opus</i> was
+                        intended to be a fortress, impregnable by inconsistencies or incompleteness. Unfortunately, even
+                        the most hardened strongholds still contain innumerable, microscopic cracks.
+                    </p>
+                    <p>
+                        When contemplating self-reference, one must consider two broad categories: reception and
+                        perception. Simple forms of feedback, such as infinitely regressing video, only receive
+                        information. They take in stimuli and echo it back after some form of transformation that is
+                        only a function of the initial input. More complex systems may gain the ability to perceive.
+                        Incoming information may be stored and later synthesized into more complex transformations on
+                        later information. These auxiliary symbols function as <i>symbols</i> that are dormant until
+                        selectively triggered by other symbols. As an example, Hofstadter describes mosquitoes as simple
+                        feedback mechanisms without access to higher-level symbols. These creatures have no need to
+                        differentiate between different types of animals or threats. To survive, it must only know to
+                        fly towards warm, flesh-like materials and to fly away from anything rapidly approaching their
+                        position that may pose a threat. As a consequence of this, Hofstadter argues that they do not
+                        have any capacity for self-reference, no "I" symbol to use in reasoning about themselves. Moving
+                        up in <i>Huneker</i> units, the text considers the dog. Through still below humans in terms of
+                        cognitive capacity, the imprint of an <i>I</i> begins to emerge. Dogs have a limited scope of
+                        symbols to draw from: their owner(s), their favorite toy, their least-favorite dog, or the
+                        postal worker. Each symbol is activated by a different set of other symbols and stimuli.
+                        Notably, the symbol repertoire of a dog has limited extensibility: most can only learn a few
+                        dozen commands and tricks before reaching their cognitive capacity. Humans, Hofstadter claims,
+                        have an <i>arbitrarily</i> extensible set of symbols. We can define a near limitless number of
+                        concepts in terms of existing symbols and learned experiences. This generality is the missing
+                        component when comparing humans to creatures of "lesses" souls.
                     </p>
                 </>
             }
