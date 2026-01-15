1aa90a2dba2c867be549d3412eb986f204effefb
matthew-pisano <matthewpisano14@gmail.com>
2026-01-14 23:51:01 -0500

    Begin Myth of Sisyphus

--- src/components/readingList/philosophyBooks.js
+++ src/components/readingList/philosophyBooks.js
@@ -1,11 +1,13 @@
 import React from "react";
 
 import nausea from "@/components/readingList/philosophy/nausea";
+import sisyphus from "@/components/readingList/philosophy/sisyphus";
 import theStranger from "@/components/readingList/philosophy/theStranger";
 
 export default (
     <>
         {theStranger}
         {nausea}
+        {sisyphus}
     </>
 );
