c5ef478a82a50e623f6426753b89efa9fc0d6b6d
matthew-pisano <matthewpisano14@gmail.com>
2026-01-15 22:11:22 -0500

    Fix Typos in Sisyphus Summary

--- src/components/readingList/philosophy/sisyphus.js
+++ src/components/readingList/philosophy/sisyphus.js
@@ -9,13 +9,13 @@ export default (
             author={"Albert Camus"}
             synopsis={
                 <>
-                    Camus' essay, <i>The Myth of Sisyphus</i>, concerns the meaning of life and the implications to this
+                    Camus' essay, <i>The Myth of Sisyphus</i>, concerns the meaning of life and the implications of this
                     ultimate question. The text first presents two options: existence has some valid meaning (subjective
                     or objective) or that we cannot justify assigning any meaning to the act of existence. To believe in
                     the former, one would need to ignore or deny the absurd. If one can see no meaning whatsoever within
                     continued existence, it can be concluded that the cessation of that existence is the only reasonable
-                    conclusion. Camus disagrees, however. Can a life be lived while fully embracing the absurd? Must we
-                    create meaning from nothing in order to justify our own continued existence to ourselves? He
+                    conclusion. Camus disagrees, however. Must we create meaning from nothing in order to justify our
+                    own continued existence to ourselves? Can a life be lived while fully embracing the absurd? He
                     believes so; the remainder of the text is dedicated to following these questions to their logical
                     implications.
                     <p>
@@ -23,21 +23,21 @@ export default (
                         its nature, the absurd is difficult to describe directly, so the text instead employs metaphor
                         to convey the concept. The vastness of the past, the ceaseless passage of time, the ultimate
                         fate that we will experience along with countless others all inspire feelings of absurdity. What
-                        meaning can we make for ourselves in a world of billions, how do our lives differ from the
+                        meaning can we make for ourselves in a world of billions? How do our lives differ from the
                         hundreds of billions that have come before us? Camus shows that this also applies on a more
                         abstract level. Scientists and philosophers are often driven by a desire for a unified truth, a
                         "nostalgia for unity". Finding this truth through theory or observation answers "how" some
                         phenomena comes into existence, but not "why" on a philosophical level. Experimentation and
                         observation can yield descriptive theories, but not prescriptive ones. We can identify what
-                        something <i>is</i>, but not what it <i>ought</i> to be.
+                        something <i>is</i>, but never what it <i>ought</i> to be.
                     </p>
                     <p>
                         There lies a contradiction at the heart of Camus' absurd. Instinctively, we are driven to search
-                        for patterns and meaning in our observations. This is contrasted by a universe that provides no
-                        rich meaning that we desire, only cold and self-perpetuating patterns
+                        for patterns and meaning in our observations. This is contrasted by a universe that does not
+                        provide the rich meaning that we desire, only cold and self-perpetuating patterns
                         <FootRef idx={1} />. The absurd is not a property of ourselves or the universe in isolation. It
-                        only appears once thw two are juxtaposed with each other. One of Camus' central points is that
-                        awareness of this contradiction is not a comfortable experience. However, any attempt to
+                        only appears once the two are juxtaposed with each other. One of Camus' central points is that
+                        being aware of this contradiction is not a comfortable experience. However, any attempt to
                         mitigate it would be simply evading the absurd, rather than acknowledging it. He asks if we can
                         live with ourselves while fully acknowledging the absurd, not whether we can bury it. Camus
                         calls the act of evading the absurd in this manner "philosophical suicide", a confession that
@@ -45,32 +45,33 @@ export default (
                     </p>
                     <p>
                         The text introduces the notion of "the absurd man". This person is fully aware of the absurd,
-                        but crucially does not deny it. For such a person, what are ones options for living? Camus
-                        states that the first choice is simply suicide as one faced with no meaning has no reason for
-                        continued existence. Camus classifies this action as a capitulation to the absurd. Like its
-                        philosophical counterpart, it declares that the absurd is simply too much to bear. Instead he
-                        identifies three paths that acknowledge the absurd without giving in to it. The first is to be
-                        in revolt against the absurd. When faced with an irrational universe and the inevitability of
-                        ones demise, the most resolute path is to live life to the fullest, to revolt against an
-                        uncaring death. It represents an acknowledgement of the absurd without allowing it to break one
-                        down. The second path is the one of true freedom. This is not only freedom in the sense that we
-                        can pursue set goals, but in a more broad sense. It is freedom from labels or roles. Embracing
-                        freedom enables ones revolt by ignoring artificial constraints. The third is the path of
-                        passion. By abandoning the notion of a set meaning for life, one also abandons a reference point
-                        for the quality of experiences. Without a universal ideal, to which scale are experiences set
-                        to? Camus instead advocates for a greater quantity and diversity of experiences in life. A
-                        person that embraces the absurd perpetually lives in the present, free from guilt or anxiety
-                        from self-imposed nostalgia or schedules. This ensures a quantity of life experiences that can
-                        serve as a psychological substitute for a higher purpose to life.
+                        but crucially does not deny it. For such a person, what are their options after confronting the
+                        absurd? Camus states that the first choice is simply to commit suicide, as one faced with a life
+                        with no meaning and therefore no reason for their continued existence. Camus classifies this
+                        action as a capitulation to the absurd. Like its philosophical counterpart, it declares that the
+                        absurd is simply too much to bear. Instead, he identifies three paths that acknowledge the
+                        absurd without giving in to it. The first is to be in revolt against the absurd. When faced with
+                        an irrational universe and the inevitability of one's demise, the most resolute path is to live
+                        life to the fullest, to revolt against an uncaring existence. It represents an acknowledgement
+                        of the absurd without allowing it to break one down. The second path is the one of true freedom.
+                        This is not only freedom in the sense that we can pursue set goals, but in a more broad sense.
+                        It is freedom from labels or roles. Embracing freedom enables one's revolt by ignoring
+                        artificial constraints like morality or religious dogma. The third is the path of passion. By
+                        abandoning the notion of a set meaning for life, one also abandons a reference point for the
+                        quality of experiences. Without a universal ideal, to which scale are experiences set to? Camus
+                        instead advocates for a greater quantity and diversity of experiences in life. A person that
+                        embraces the absurd perpetually lives in the present, free from guilt or anxiety from
+                        self-imposed nostalgia or guilt. This ensures a quantity of life experiences that can serve as a
+                        psychological substitute for a higher purpose to life.
                     </p>
                     <p>
                         Camus sees three archetypes that emulate the absurd principles of revolt, freedom, and passion:
-                        the seducer, the actor, and the conquerer. Though he does not directly endorse any of these
+                        The Seducer, The Actor, and The Conquerer. Though he does not directly endorse any of these
                         three lifestyles, they help to illustrate his idea of "the absurd man". These roles are free
                         from traditional morals, only guided by their own integrity and internal moral compas, free from
-                        religious or cultural pressures. The fictional character Don Juan embodies Camus' seduces. He
+                        religious or cultural pressures. The fictional character Don Juan embodies Camus' seducer. He
                         moves from woman to woman, convincing them to fall for him with the same repetitions of
-                        techniques and never stating for too long with any of them. Don Juan lives for his own form of
+                        techniques and never staying for too long with any of them. Don Juan lives for his own form of
                         conquest, unconcerned with the wellbeing of others or the implications of his actions. The actor
                         also embraces the absurd. Throughout their career, an actor dons countless roles, experiencing
                         the world through the eyes of their character. Unlike authors, Camus argues, the fame and
@@ -81,25 +82,25 @@ export default (
                         Camus' ideas at their most intense. The conquerer knows that countless leaders came before them
                         and countless came after; there is no illusion of eternal empire or legacy. Navigating the risky
                         world of politics and warfare, the conqueror must live their life at the highest intensity, lest
-                        they be consumed by those around them. The act of political or military revolt is therefore tied
-                        to a revolt against the absurd so long as the participants are aware of its futility yet push
-                        onward regardless.
+                        they be conquered themselves. The act of political or military revolt is therefore tied to a
+                        revolt against the absurd so long as the participants are aware of its futility, yet push onward
+                        regardless.
                     </p>
                     <p>
                         In previous sections, Camus has described his absurd man as someone who lives a life of
-                        contradiction: they live a full and intense life, fully aware of the meaninglessness of their
-                        actions. Camus connects this lifestyle to the of the artist, writers of fiction in particular.
-                        These people create entire worlds of their own, fleshing out characters, plots, and environments
-                        while fully ware of its detachment from reality. This fiction cannot add anything real to base
-                        reality, but can enrich its experience and convey a point of view to the audience. Art cannot
-                        transcend the meaninglessness of life, but it can form a revolt against it as the artist asserts
-                        their own perspective upon the world.
+                        contradiction: they live a full and intense life, while still aware of the meaninglessness of
+                        their actions. Camus connects this lifestyle to that of the artist (writers of fiction in
+                        particular). These people create entire worlds of their own, fleshing out characters, plots, and
+                        environments while fully aware of its detachment from reality. This fiction cannot add anything
+                        real to base reality, but can enrich its experience and convey a point of view to the audience.
+                        Art cannot transcend the meaninglessness of life, but it can form a revolt against it as the
+                        artist asserts their own perspective upon the world.
                     </p>
                     <p>
-                        The absurd artist must continue their work fully aware that it is ultimately in vain. Camus
+                        The absurd artist must continue their work, fully aware that it is ultimately in vain. Camus
                         asserts that, if there is no life after death, one can only assert themselves to the world
                         around them during their natural life. This act requires discipline and the ability to resist
-                        notions of legacy or salvation. An artists work exists to present their perspective to the
+                        notions of legacy or salvation. An artist's work exists to present their perspective to the
                         greater world, but not to make grand statements about its underlying meaning. For the absurd
                         man, the nature of the universe forbids such statements from ever holding any real meaning. To
                         Camus, life ends with the finality of death, but any time before then is fully up to our
@@ -108,24 +109,24 @@ export default (
                     </p>
                     <p>
                         Camus ends his essay with the myth of Sisyphus itself
-                        <FootRef idx={2} />. Sisyphus, the man, was the King of Ephyra, known for his greed, cunning,
-                        and trickery. Sisyphus blackmails Zeus into granting his city a natural spring for water.
-                        Enraged, Zeus banishes him to the underworld for his treachery. Here, he tricks both Thanatos
-                        and Persephone, allowing him to cheat death twice. Eventually he is dragged back ti the
-                        underworld and condemned to his eternal punishment. Here, he is doomed to roll a rock up to the
-                        top of a mountain every day, only for it to roll back down as soon as it reaches the top in a
-                        never ending cycle. The gods reasoned that this eternal, futile labor constituted the most
-                        insulting of punishments. Camus sees this differently, however. To him, Sisyphus is an absurdist
-                        archetype. His trickery of the gods and determination to live for himself embodies Camus' absurd
-                        man. As he toils in the underworld, he is conscious and aware of his eternal fate. Specifically,
-                        Camus is interested in his state of mind just after the rock begins to roll back down the
-                        mountain. As he climbs back down, he contemplates the absurdity of his life after death. He
-                        understands the futility of his work, yet strains against the rock day after day regardless. Any
-                        feelings of sorrow that he may retain are tied to his nostalgia for his previous life and his
-                        hope for an end to his condemnation. Accepting his fate, Camus argues, frees Sisyphus from these
-                        feelings. As Sisyphus rolls the rock up the mountain every day, his only option is to approach
-                        his work with joy after he has accepted the absurdity of his condition. To Camus, there is only
-                        one rational conclusion: that one must imagine Sisyphus happy.
+                        <FootRef idx={2} />. Sisyphus, the man, was once the King of Ephyra, known for his greed,
+                        cunning, and trickery. According to some Greek legends, Sisyphus blackmails Zeus into granting
+                        his city a natural spring for water. Enraged, Zeus banishes him to the underworld for his
+                        treachery. Here, he tricks both Thanatos and Persephone, allowing him to cheat death twice.
+                        Eventually he is dragged back to the underworld and condemned to his eternal punishment. Here,
+                        he is doomed to roll a rock up to the top of a mountain every day, only for it to roll back down
+                        as soon as it reaches the top. The gods reasoned that this eternal, futile labor constituted the
+                        most insulting of punishments. Camus sees this differently, however. To him, Sisyphus is an
+                        absurdist archetype. His trickery of the gods and determination to live for himself embodies
+                        Camus' absurd man. As he toils in the underworld, he is conscious and aware of his eternal fate.
+                        Specifically, Camus is interested in his state of mind just after the rock begins to roll back
+                        down the mountain. As he climbs back down, he contemplates the absurdity of his life after
+                        death. He understands the futility of his work, yet strains against the rock day after day
+                        regardless. Any feelings of sorrow that he may retain are tied to his nostalgia for his previous
+                        life and his hope for an end to his condemnation. Accepting his fate, Camus argues, frees
+                        Sisyphus from these feelings. As Sisyphus rolls the rock up the mountain every day, his only
+                        option is to approach his work with joy after he has accepted the absurdity of his condition. To
+                        Camus, there is only one rational conclusion: that one must imagine Sisyphus happy.
                     </p>
                 </>
             }
