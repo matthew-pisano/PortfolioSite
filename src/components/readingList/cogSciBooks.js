1d2c1ad71f25df0ebc5075f4e8756d92e5f5c202
matthew-pisano <matthewpisano14@gmail.com>
2026-02-10 21:06:48 -0500

    Update Human Compatible

--- src/components/readingList/cogSciBooks.js
+++ src/components/readingList/cogSciBooks.js
@@ -1,12 +1,12 @@
 import React from "react";
 
-// import alignmentProblem from "@/components/readingList/cogsci/alignmentProblem";
-// import behave from "@/components/readingList/cogsci/behave";
-// import determined from "@/components/readingList/cogsci/determined";
-// import emotionMachine from "@/components/readingList/cogsci/emotionMachine";
-// import godelEscherBach from "@/components/readingList/cogsci/godelEscherBach";
-// import humanCompatible from "@/components/readingList/cogsci/humanCompatible";
-// import strangeLoop from "@/components/readingList/cogsci/strangeLoop";
+import alignmentProblem from "@/components/readingList/cogsci/alignmentProblem";
+import behave from "@/components/readingList/cogsci/behave";
+import determined from "@/components/readingList/cogsci/determined";
+import emotionMachine from "@/components/readingList/cogsci/emotionMachine";
+import godelEscherBach from "@/components/readingList/cogsci/godelEscherBach";
+import humanCompatible from "@/components/readingList/cogsci/humanCompatible";
+import strangeLoop from "@/components/readingList/cogsci/strangeLoop";
 import superintelligence from "@/components/readingList/cogsci/superintelligence";
 import thinkingFastAndSlow from "@/components/readingList/cogsci/thinkingFastAndSlow";
 
@@ -14,12 +14,12 @@ export default (
     <>
         {superintelligence}
         {thinkingFastAndSlow}
-        {/*{humanCompatible}*/}
-        {/*{alignmentProblem}*/}
-        {/*{godelEscherBach}*/}
-        {/*{strangeLoop}*/}
-        {/*{determined}*/}
-        {/*{behave}*/}
-        {/*{emotionMachine}*/}
+        {humanCompatible}
+        {alignmentProblem}
+        {godelEscherBach}
+        {strangeLoop}
+        {determined}
+        {behave}
+        {emotionMachine}
     </>
 );
