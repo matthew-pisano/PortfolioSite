75c7c00a30ff2e19ad0e41f5f47ab7a392eec093
matthew-pisano <matthewpisano14@gmail.com>
2026-02-22 14:45:22 -0500

    Update Middleware to Proxy

--- src/proxy.js
+++ src/proxy.js
@@ -1,7 +1,7 @@
 import { cookies } from "next/headers";
 import { NextResponse } from "next/server";
 
-export async function middleware(request) {
+export async function proxy(request) {
     const cookieStore = await cookies();
     const authToken = cookieStore.get("mathesisAdminAuth");
     let hasAdminAuth = authToken && authToken.value === "screwball";
