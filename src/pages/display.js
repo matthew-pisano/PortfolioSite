6fba586a052d56dbe59c4df1218bf4e50929e8b9
matthew-pisano <matthewpisano14@gmail.com>
2026-02-22 18:37:05 -0500

    Isolate Display Page Elements

--- src/pages/display.js
+++ src/pages/display.js
@@ -1,35 +1,38 @@
-import React, { useEffect, useState } from "react";
-
-import parse from "html-react-parser";
+import React, { useEffect } from "react";
 
 import Wrapper, { PageInfo } from "@/components/wrappers/Wrapper";
 import { masterFileSystem } from "@/lib/fileSystem/fileSystem";
 import { Perms } from "@/lib/fileSystem/fileSystemMeta";
 import { Directory } from "@/lib/fileSystem/fileSystemObjects";
+import styles from "@/styles/wrappers/DisplayWrapper.module.css";
 
 export default function Display() {
-    const [pageText, setPageText] = useState("");
     let pageInfo = new PageInfo("display", "Display", "Displaying the contents of a file");
 
     useEffect(() => {
         let filePath = new URLSearchParams(window.location.search).get("file");
+        let displayFrame = document.getElementById("displayFrame");
         if (!filePath) {
-            setPageText("No file specified!");
+            displayFrame.srcdoc = "No file specified!";
             return;
         }
 
         let currentFile = masterFileSystem.getItem(filePath);
         // Show error message if file is not found, is a directory, or does not have read permissions
-        if (!currentFile) setPageText(`Cannot find file at ${filePath}!`);
-        else if (currentFile instanceof Directory) setPageText("Cannot open a directory!");
+        if (!currentFile) displayFrame.srcdoc = `Cannot find file at ${filePath}!`;
+        else if (currentFile instanceof Directory) displayFrame.srcdoc = "Cannot open a directory!";
         else if (!(currentFile.permission & Perms.READ))
-            setPageText(`Insufficient permissions to access source of ${filePath}!`);
+            displayFrame.srcdoc = `Insufficient permissions to access source of ${filePath}!`;
         else {
             // Load the file text into the page
             let pageText = currentFile.text();
-            setPageText(pageText ? pageText : "<h3><i>[Empty file]</i></h3>");
+            displayFrame.srcdoc = pageText ? pageText : "<h3><i>[Empty file]</i></h3>";
         }
     }, []);
 
-    return <Wrapper pageName={pageInfo.pageName}>{parse(pageText)}</Wrapper>;
+    return (
+        <Wrapper pageName={pageInfo.pageName} pageClass={styles.displayPage}>
+            <iframe id="displayFrame" className={styles.displayFrame} frameBorder="0"></iframe>
+        </Wrapper>
+    );
 }
