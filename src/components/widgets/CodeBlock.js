68bd5895f91888c10768535d572e09598c56aa6d
matthew-pisano <matthewpisano14@gmail.com>
2026-05-05 20:02:43 -0400

    Add Comments to Code block

--- src/components/widgets/CodeBlock.js
+++ src/components/widgets/CodeBlock.js
@@ -13,6 +13,15 @@ SyntaxHighlighter.registerLanguage("cmake", cmake);
 SyntaxHighlighter.registerLanguage("cpp", cpp);
 SyntaxHighlighter.registerLanguage("python", python);
 
+/**
+ * A code block element which ensures code overflows instead of restricting responsive width.
+ * @param children The code text
+ * @param language The language of the code, for syntax highlighting
+ * @param style Any custom style elements
+ * @param props Any extra props to add
+ * @returns {JSX.Element} A responsive code block element
+ * @constructor
+ */
 export default function CodeBlock({ children, language = "text", style = dracula, ...props }) {
     return (
         <div style={{ width: 0, minWidth: "100%", overflow: "auto" }}>
