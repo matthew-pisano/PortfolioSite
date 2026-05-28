2041dd342adcdc3c2f19c03b5e6f6d28e4ca66ab
matthew-pisano <matthewpisano14@gmail.com>
2026-05-27 20:35:03 -0400

    Update Sapiens Summary

--- src/components/readingList/cogsci/sapiens.js
+++ src/components/readingList/cogsci/sapiens.js
@@ -91,7 +91,123 @@ export default (
                     the most hardened band of a few dozen Neanderthals. We did not out-smart or physically overpower
                     other humans, we simply out-communicated them.
                 </p>
-                <p>Even after the cognitive revolution, tribes of Sapiens were overwhelmingly hunter-gathers.</p>
+                <p>
+                    Even after the cognitive revolution, tribes of Sapiens were overwhelmingly hunter-gathers. They
+                    worked relatively little, gathered what they found, and hunted to make up for any difference. If the
+                    land became spare with resources, they simply moved somewhere else. While it is difficult to be
+                    sure, violence was also likely relatively rare. While there are sites of notable hunter-gather
+                    violence, like Jebel Sahaba in Sudan or Nataruk in Kenya, these appear to be relatively rare and
+                    fought primarily over local resources.
+                </p>
+                <p>
+                    Around 10,000 BCE, this began to change with the dawn of agriculture in mesopotamia. Instead of
+                    freely moving about a large area in search of resources, humans began to stay in a single location
+                    and create the resources themselves. This is when we see evidence for the majority of domesticated
+                    plants and animals
+                    <Footnote>
+                        Largely the exact same ones we still see today, in fact. As it turns out, there are relatively
+                        few species that are easy to domesticate and maintain. Early humans largely found what they
+                        needed from the outset.
+                    </Footnote>
+                    . In the naive view of history, the Neolithic revolution is usually presented as a collective
+                    triumph for humanity. Instead of needing to expend precious energy scouring the land for resources,
+                    we could simply relax and wait for the resources to come to us!
+                </p>
+                <p>
+                    However, the evidence paints a very different picture of how this shift was experienced by real
+                    humans at the time. Compared to contemporary nomadic tribes, our agricultural ancestors worked
+                    longer, had a less diverse diet, and were much more vulnerable to disease, famine, war, and a host
+                    of other calamities. As Harari emphasizes, this was a pretty raw deal for those living in villages.
+                    Why did this even happen in the first place, then. Nobody would willingly subject themselves and
+                    their progeny to such conditions if informed of how they would turn out. That is the key, though.
+                    This was a very gradual process, happening over hundreds of years, and each step looked like a
+                    relative improvement over before. If a particular field was unusually plentiful with berries and
+                    grasses, a tribe may decide to stay near throughout the winter and return in the spring. Eventually,
+                    they may have realized that some of their previous harvest was lost on the ground and regrew into
+                    even more plants next season. Perhaps burying the seeds produced even better results! On and on this
+                    went as groups became tied even tighter to specific areas of land. By the time each stage had
+                    completed, nobody was able to remember that any previous stage ever existed. Our myopia has a
+                    history as old as our own. This may not have been entirely accidental too. Sites like Göbeklitepe
+                    indicate that early farmers may have endured this harsher life in service of strong religious or
+                    cultural beliefs. A direct consequence of the last revolution.
+                </p>
+                <p>
+                    As farming became more firmly entrenched, stresses upon farmers grew. Anxiety over being eaten by a
+                    stalking lion at worst lasted a few hours, but anxiety around whether crops would fail next season
+                    would last months. To help cope with these stresses, people invented new myths: societies. Early
+                    governments acted as a method for distributing resources, deciding what to store and what to
+                    consume. A failed harvest impacted entire villages, not just individual farmers. This meant that
+                    villagers were not just worried for themselves, but for their community as a while, encouraging
+                    strong beliefs in a shared culture and the burgeoning political leadership. Around this time,
+                    shortly after agriculture took hold, we begin to see evidence of well-defined elites in farming
+                    cultures. Administration was a fair amount of work, and thus necessitated that the administrators
+                    forego farming themselves, instead relying on the administrated to feed them. From the neolithic to
+                    the industrial revolution, around ninety percent of people remained farmers while the remaining ten
+                    percent fell into various sub-categories of elites. "History is something that very few people have
+                    been doing while everyone else was ploughing fields and carrying water buckets".
+                </p>
+                <p>
+                    A point that this book stresses is that the most important ingredient for a society's success is not
+                    a strong people or plentiful resources. Instead, it is belief in a set of common myths. These
+                    include more traditional definitions like religion or cultural identity, but also includes the myth
+                    of society, money, and collective trust. Without these strong myths, even resource-rich societies
+                    crumble.
+                </p>
+                <p>
+                    Tribes and villages required very little administration. At most you may have some central
+                    leadership, a handful of warriors, and village elders. Agriculture functions as a sort of cultural
+                    attractor. Once a society settles down, it tends to grow, and fast. Settled areas of land can
+                    support far more humans than those populated by nomads. These agricultural societies expanded to
+                    more and more land, absorbing or displacing any nomadic tribes they encountered. As population
+                    ballooned, a lightweight administration was no longer sufficient to organize tens of thousands of
+                    people and to ensure that they all cooperate. Thus, bureaucracy was born. The rules of such large
+                    societies began to sharply diverge from the instinctual rules that <i>Homo sapiens</i> has lived by
+                    for hundreds of millennia. Bureaucrats were individuals who were specifically accustomed to these
+                    rules and served to interpret and administer laws. Still, bureaucrats were still humans and had
+                    limited memories and recall abilities. The invention of the bureaucrat coincided with another
+                    invention: writing.
+                </p>
+                <p>
+                    The maintenance, organization, and retrieval of written documents soon began to occupy the time of
+                    bureaucrats the ancient world over. As land changed hands, laws were amended, and history
+                    progressed, more and more records were kept. This soon turned the once simple act of retrieval into
+                    a complex problem which required deliberate planning to overcome. From within our own minds, we can
+                    effortlessly summon nearly any piece of information from our memories almost effortlessly. What are
+                    the names of your parents? Which great conquerer founded your home empire? What is the minimum
+                    number of goat sacrifices are usually needed to appease your local rain god? Despite the vast hordes
+                    of knowledge each mind contained, retrieval oftentimes took no longer than a moment. A vast archive
+                    overflowing with bunky clay tablets was another story entirely. The need to organize and retrieve
+                    data in this very physical manner necessitated ever more complex organization and indexing schemes.
+                    Unfortunately, the full script of language was unsuitable for conversing concise numerical
+                    information. This changed sometime in the ninth centry CE, as Arab scholars first began to adopt a
+                    strange limited script from newly conquered Indian provinces. It contained far fewer characters than
+                    standard Arabic and was only useful when describing very specific things. While this sounds rather
+                    limiting, the scholars and bureaucrats of the Abbasid Caliphate quickly realized their usefulness
+                    for indexing and calculation. We may recognize these odd symbols as 0, 1, 2, 3, 4, 5, 6, 7, 8, 9.
+                </p>
+                <p>
+                    Shifting focus towards the evolution of culture over bureaucracy, <i>Sapiens</i> whether there are
+                    patterns in cultural changes or if they largely occur at random. One thing that is for certain is
+                    that these cultures do indeed change. When the first Tasmanians arrived 40,000 years ago from across
+                    a land bridge to Australia, they brought their native culture with them. However, if you were to
+                    introduce one of these founding Tasmanians to their ancestors living in 1641, just before first
+                    contact with Dutch sailors, they would have struggled to find any similarities between this alien
+                    culture and their own! Even in total isolation from the outside world, human cultures shift an
+                    evolve. Smaller cultures coalesce into larger ones, and well-established cultures splinter into more
+                    localized tribes. However, Harari argues, there is still a pattern to be seen here. Not from merely
+                    a birds-eye view, nor even a ten-thousand foot view. Rather, form the view of some cultural spy
+                    satellite, a general pattern begins to emerge. Buy-in-large, many isolated cultures gradually begin
+                    to unify into much larger ones. In the sixth centura CE, Arabia was populated by hundreds of
+                    isolated Bedouin tribes, each with a unique dialect and culture. Less than a centry later, they were
+                    nearly all unified under the unitary Rashidun Caliphate. While there are countless counter-examples
+                    to this trend, such as how the once mighty Mongol empire shattered into dozens of autonomous
+                    Khanates, there is a trend nonetheless. Thousands of years ago, the world was made up of thousands
+                    of cultural islands, almost entirely isolated from each other. Each with their own time-keeping
+                    systems, languages, and universal myths. Today, there are still many distinct cultures, but the
+                    number has shrunk from many hundreds of thousands to a few hundred major cultures. Additionally,
+                    while religious and moral disagreements are common, most well-connected cultures can readily agree
+                    upon basic measurement standards or the fundamental truths of the universe.
+                </p>
             </BookTileSynopsis>
             <BookTileThoughts>
                 <p></p>
