7afd42a709743790d6db43b87796c81e4dda5dac
matthew-pisano <matthewpisano14@gmail.com>
2026-07-07 23:43:31 -0400

    Add New Blog Page

---
+++ src/pages/works/blog/on-arbitrary-cognitive-execution.js
@@ -0,0 +1,35 @@
+import React from "react";
+
+import Link from "next/link";
+
+import { BlogSection } from "@/components/widgets/BlogSection";
+import CodeBlock from "@/components/widgets/CodeBlock";
+import { FootnoteProvider, Footnote, FootnoteList } from "@/components/widgets/FootNote";
+import BlogWrapper, { BlogInfo } from "@/components/wrappers/BlogWrapper";
+import { genPageTitle } from "@/lib/util/utils";
+
+const blogInfo = new BlogInfo(
+    "On the Possibility of Arbitrary Cognitive Execution",
+    "An inquiry into the exploitation of cognitive computation",
+    new Date(2026, 6, 7),
+    "arbitraryCognition",
+    new Date(2026, 6, 7)
+);
+
+export default function ArbitraryCognition() {
+    return (
+        <BlogWrapper
+            pageName={genPageTitle(__filename)}
+            title={blogInfo.title}
+            subtitle={blogInfo.subtitle}
+            pubDate={blogInfo.pubDate}
+            modDate={blogInfo.modDate}>
+            <FootnoteProvider label={blogInfo.anchor}>
+                <hr />
+                <FootnoteList />
+            </FootnoteProvider>
+        </BlogWrapper>
+    );
+}
+
+export { blogInfo };
