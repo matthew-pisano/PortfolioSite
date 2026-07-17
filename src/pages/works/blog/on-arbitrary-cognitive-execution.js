6e2dc7fff5966984afbcedfbce7523df09daea5a
matthew-pisano <matthewpisano14@gmail.com>
2026-07-16 23:23:15 -0400

    Finish Introduction

--- src/pages/works/blog/on-arbitrary-cognitive-execution.js
+++ src/pages/works/blog/on-arbitrary-cognitive-execution.js
@@ -140,6 +140,59 @@ export default function ArbitraryCognition() {
                     register machine, but it could just as easily be the execution environment of a machine learning
                     model or any other computational substrate.
                 </p>
+                <BlogSection level={2}>Cognition as Computation</BlogSection>
+                <p>
+                    One such computational substrate could potentially be the human brain. However, to further pursue
+                    this line of inquiry, we must make an assumption: that the human brain is, at least to some extent,
+                    a computing device. We will touch on how valid or invalid this assumption may be later but, for now,
+                    we will accept this premise for the sake of exploration.
+                </p>
+                <p>
+                    What does it mean for a system to be a computing device? I have previously approached this from the
+                    high-level angle of simulating Turing machines in my work{" "}
+                    <Link href={"https://matthewpisano.com/works/blog/on-general-transformers"}>
+                        On Generally Intelligent Transformers
+                    </Link>
+                    , but we can arrive at a more precise definition by asking which behaviors we expect a computing
+                    device to express. To extract useful computations, we need a system with a well-defined interface
+                    for input and output. A system which could take input at any point and give output at any point is
+                    less of a device and more of an exposition of raw physical laws. The restriction of manner and
+                    format of input and output greatly simplifies the design process and eliminates ambiguity. We also
+                    our computing device to transform patterns of input information into different patterns of output
+                    information. A system which always echoes its input as output does not add any information of its
+                    own and is therefore not very useful. This manipulation must be predictable, though not necessarily
+                    deterministic. The use case of a computing device is to add meaningful
+                    <Footnote>
+                        <i>Meaning</i> and <i>information</i> may seem like orthogonal concepts from an information
+                        theory perspective, but when speaking of computing devices in this manner, we are speaking
+                        relative to what we humans generally consider to be useful and interesting. From the perspective
+                        of information, a random number generator is maximally informative, yet its output is generally
+                        uninteresting to a human. We expect our computing devices to manipulate the information in out
+                        input without completely removing it or fully maximizing it. Therefore, this definition will
+                        contain some human ambiguities.
+                    </Footnote>{" "}
+                    information to some input as output. If the output does not follow from the input in a manner
+                    meaningfully related to the computing device's task, then it is not a useful device. Furthermore,
+                    while classical computers are designed to be deterministic, others such as quantum computers are
+                    not. However, both are designed to be predictable (to some extent), conditional on an input. We also
+                    expect our computing devices to be capable of representing a wide variety of tasks, while being
+                    selectively tunable to particular tasks as needed. Many physical systems fulfill the former criteria
+                    well: they can manipulate inputs into outputs in a controlled and predictable manner. For example,
+                    the physical system of a cooling cup of tea is useful for simulating the physical processes present
+                    in ... a cooling cup of tea. This system can only represent one class of configurations: itself. A
+                    cooling cup of tea cannot, for instance, be tuned into modeling a cooling cup of coffee without
+                    being irreversibly turned into that system. In contrast, a computer can simulate both, a game of
+                    chess, or a webpage.
+                </p>
+                <p>
+                    Considering these requirements, we can put together a more precise definition. We can conceptualize
+                    a computing device as any system which takes in patterns of information as input, manipulates this
+                    information in a predictable manner, dependent on a task-oriented internal configuration, and
+                    outputs different patterns of information. This definition encapsulates classical and quantum
+                    computers in general, along with specialized systems such as artificial or even biological neural
+                    networks. Most importantly, this definition gives us a way to now only describe how a system behaves
+                    normally, but also how it could be exploited to behave abnormally.
+                </p>
                 <hr />
                 <FootnoteList />
             </FootnoteProvider>
